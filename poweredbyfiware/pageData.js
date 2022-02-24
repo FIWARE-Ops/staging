54e7145f4a29ec2916467574624fd2d9cc6f025f
Giacomo De Panfilis <reddy.depa@gmail.com>
2022-02-24 19:07:51 +0100

    pageData uploaded

---
+++ poweredbyfiware/pageData.js
@@ -0,0 +1,133 @@
+// pageData
+var pageData = [
+  {
+    name: "WiseTown",
+    img:
+      "https://www.fiware.org/wp-content/uploads/logo_WiseTown.jpg",
+    company: "TeamDev",
+    fiwareMember: true,
+    companyLink: "https://www.fiware.org/solution-test/",
+    domain: ["Smart Cities", "Smart Mobility"],
+    type: "Solutions",
+    technology: ["IoT", "Blockchain"],
+    year: 2012,
+    content:
+      "WiseTown uses crowd-sourcing, open data, IoT and social to provide decision support systems to city managers and to improve citizens’ quality of life."
+  },
+  {
+    name: "Agricolus",
+    img:
+      "https://www.fiware.org/wp-content/uploads/logo_Agricolus.jpg",
+    company: "Agricolus",
+    fiwareMember: true,
+    companyLink: "https://www.fiware.org/lorem-ipsum-dolor-sit-amet-consectetuer-adipiscing-elit-aenean-test-2/",
+    domain: ["Smart AgriFood", "Smart AgriTech"],
+    type: "Solutions",
+    technology: ["IoT", "technology2", "technology4"],
+    year: 2017,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "Cloud City Operations Center",
+    img:
+      "https://www.fiware.org/wp-content/uploads/logo_Nec.jpg",
+    company: "NEC",
+    fiwareMember: true,
+    companyLink: "https://www.fiware.org/lorem-ipsum-dolor-sit-amet-consectetuer-adipiscing-elit-aenean-test-3/",
+    domain: ["Smart Cities", "Smart Industry", "Smart Ports"],
+    type: "Solutions",
+    technology: ["IoT", "Big Data"],
+    year: 2018,
+    content:
+      "CCOC acts as the ‘brain’ of the city, able to “listen” and “comprehend” what is happening all over the city to thus make better decisions and provide the right information to its inhabitants."
+  },
+  {
+    name: "Tytire tu patulae",
+    img:
+      "https://fundingbox-files.s3-eu-central-1.amazonaws.com/growabric/yBQdM3KybF/logo_growabric_small.png",
+    company: "Tik Tok",
+    fiwareMember: false,
+    companyLink: "https://www.udgalliance.org/",
+    domain: ["Smart Cities", "Smart Mobility"],
+    type: "Solutions",
+    technology: ["technology12", "technology2", "technology1"],
+    year: 2017,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "4 Lorem ipsum dolor sit amet tytire tu patulae recubans",
+    img:
+      "https://www.fiware.org/wp-content/uploads/logo_Nec.jpg",
+    company: "Pinterest",
+    fiwareMember: true,
+    companyLink: "https://www.udgalliance.org/",
+    domain: ["Smart Cities", "Smart Mobility"],
+    type: "Platform",
+    technology: ["Lorem ipsum1", "Lorem ipsum2", "Lorem ipsum3"],
+    year: 2020,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "5 Lorem ipsum dolor sit amet tytire tu patulae recubans",
+    img:
+      "https://fundingbox-files.s3-eu-central-1.amazonaws.com/teamdev/logo_TD_2015_OK.png",
+    company: "Flickr",
+    fiwareMember: true,
+    companyLink: "https://www.udgalliance.org/",
+    category: "Lorem ipsum",
+    domain: ["Smart", "Smart Mobility"],
+    type: "Solutions",
+    technology: ["Lorem ipsum1", "technology2", "technology3"],
+    year: 2021,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "6 Lorem ipsum dolor sit amet tytire tu patulae recubans",
+    img:
+      "https://fundingbox-files.s3-eu-central-1.amazonaws.com/growabric/yBQdM3KybF/logo_growabric_small.png",
+    company: "Meta",
+    fiwareMember: true,
+    companyLink: "https://www.udgalliance.org/",
+    category: "Lorem ipsum",
+    domain: ["Smart Cities", "Mobility"],
+    type: "Solutions",
+    technology: ["Lorem ipsum3", "Lorem ipsum", "Lorem ipsum4"],
+    year: 2020,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "7 Lorem ipsum dolor sit amet tytire tu patulae recubans",
+    img:
+      "https://fundingbox-files.s3-eu-central-1.amazonaws.com/growabric/yBQdM3KybF/logo_growabric_small.png",
+    company: "Instagram",
+    fiwareMember: true,
+    companyLink: "https://www.udgalliance.org/",
+    category: "Lorem ipsum",
+    domain: ["Smart Cities", "Smart Mobility"],
+    type: "Platform",
+    technology: ["Lorem ipsum", "Lorem ipsum", "Lorem ipsum"],
+    year: 2020,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  },
+  {
+    name: "8 Lorem ipsum dolor sit amet tytire tu patulae recubans",
+    img:
+      "https://fundingbox-files.s3-eu-central-1.amazonaws.com/enmarinas/T8HDrfnHxG/rgb_png_necblue_l.png",
+    company: "Engineering",
+    fiwareMember: true,
+    companyLink: "https://www.udgalliance.org/",
+    category: "Lorem ipsum",
+    domain: ["Smart Cities", "Smart Mobility"],
+    type: "Platform",
+    technology: ["Lorem ipsum", "Lorem ipsum", "Lorem ipsum"],
+    year: 2021,
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
+  }
+];
