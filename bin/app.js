6f5f016378060f5e46c594bd634afa8d4d43301a
Giacomo De Panfilis <reddy.depa@gmail.com>
2022-05-31 15:14:51 +0200

    Updating products

--- bin/app.js
+++ bin/app.js
@@ -8,54 +8,56 @@ input.forEach(item => {
   const domains = [];
   const tech = [];
 
-  item.Domains.split(',').forEach(item => {
-    if (item.trim() !== '') {
-      domains.push(item.trim());
+  if (item.Status !== 'Deleted') {
+    item.Domains.split(',').forEach(item => {
+      if (item.trim() !== '') {
+        domains.push(item.trim());
+      }
+    });
+    item.Domains = domains;
+
+    item.Technologies.split(',').forEach(item => {
+      if (item.trim() !== '') {
+        tech.push(item.trim());
+      }
+    });
+    item.Technologies = tech;
+
+    if (item.Member === 'False') {
+      item.Member = false;
     }
-  });
-  item.Domains = domains;
-
-  item.Technologies.split(',').forEach(item => {
-    if (item.trim() !== '') {
-      tech.push(item.trim());
+    if (item.Member === 'True') {
+      item.Member = true;
     }
-  });
-  item.Technologies = tech;
+    if (item.iHub === 'False') {
+      item.iHub = false;
+    }
+    if (item.iHub === 'True') {
+      item.iHub = true;
+    }
 
-  if (item.Member === 'False') {
-    item.Member = false;
-  }
-  if (item.Member === 'True') {
-    item.Member = true;
-  }
-  if (item.iHub === 'False') {
-    item.iHub = false;
-  }
-  if (item.iHub === 'True') {
-    item.iHub = true;
-  }
+    const obj = {};
+    obj.company = item['Organisation Name'];
+    obj.name = item['Product Name'];
+    obj.img = item['Logo'];
+    obj.fiwareMember = item.Member;
+    obj.fiwareIhub = item.iHub;
+    obj.companyLink = item['Product Website'];
+    obj.domain = item['Domains'];
+    obj.type = item['Type of Product'];
+    obj.technology = item['Technologies'];
+    obj.year = item['Certified in'];
+    obj.content = item['Excerpt'];
 
-  const obj = {};
-  obj.company = item['Organisation Name'];
-  obj.name = item['Product Name'];
-  obj.img = item['Logo'];
-  obj.fiwareMember = item.Member;
-  obj.fiwareIhub = item.iHub;
-  obj.companyLink = item['Product Website'];
-  obj.domain = item['Domains'];
-  obj.type = item['Type of Product'];
-  obj.technology = item['Technologies'];
-  obj.year = item['Certified in'];
-  obj.content = item['Excerpt'];
-
-  if (item['Category'] === 'Powered by FIWARE') {
-    poweredBy.push(obj);
-  } else if (item['Category'] === 'FIWARE-Ready') {
-    ready.push(obj);
-  } else if (item['Category'] === 'Services') {
-    service.push(obj);
-  } else {
-    console.log(item['Category']);
+    if (item['Category'] === 'Powered by FIWARE') {
+      poweredBy.push(obj);
+    } else if (item['Category'] === 'FIWARE-Ready') {
+      ready.push(obj);
+    } else if (item['Category'] === 'Services') {
+      service.push(obj);
+    } else {
+      console.log(item['Category']);
+    }
   }
 });
 
