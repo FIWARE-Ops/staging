84d98a7ee5489260d6275d160b70c95b41a6a44a
Jason Fox <jason.fox@fiware.org>
2022-05-31 16:48:42 +0200

    Switch to CSV input.

--- bin/app.js
+++ bin/app.js
@@ -1,121 +1,114 @@
 const fs = require('fs');
-const input = JSON.parse(fs.readFileSync('untitled.txt', 'utf8'));
+const csvFilePath = 'untitled.txt';
+const csv = require('csvtojson');
 const poweredBy = [];
 const ready = [];
 const service = [];
 const cities = [];
 
-input.forEach(item => {
-  const domains = [];
-  const tech = [];
+csv()
+  .fromFile(csvFilePath)
+  .then(input => {
+    input.forEach(item => {
+      const domains = [];
+      const tech = [];
 
-  const searchMask = 'fiware';
-  const regEx = new RegExp(searchMask, 'ig');
-  const replaceMask = 'FIWARE';
+      const searchMask = 'fiware';
+      const regEx = new RegExp(searchMask, 'ig');
+      const replaceMask = 'FIWARE';
 
-  if (item.Status !== 'Deleted') {
-    item.Domains.split(',').forEach(item => {
-      if (item.trim() !== '') {
-        domains.push(item.trim());
+      if (item.Status !== 'Deleted') {
+        item.Domains.split(',').forEach(item => {
+          if (item.trim() !== '') {
+            domains.push(item.trim());
+          }
+        });
+        item.Domains = domains;
+
+        item.Technologies.split(',').forEach(item => {
+          if (item.trim() !== '') {
+            tech.push(item.trim());
+          }
+        });
+        item.Technologies = tech;
+
+        if (item.Member === 'False') {
+          item.Member = false;
+        }
+        if (item.Member === 'True') {
+          item.Member = true;
+        }
+        if (item.iHub === 'False') {
+          item.iHub = false;
+        }
+        if (item.iHub === 'True') {
+          item.iHub = true;
+        }
+
+        const obj = {};
+        obj.company = item['Organisation Name'];
+        obj.name = item['Product Name'];
+        obj.img = item['Logo'];
+        obj.fiwareMember = item.Member;
+        obj.fiwareIhub = item.iHub;
+        obj.companyLink = item['Product Website'];
+        obj.domain = item['Domains'];
+        obj.type = item['Type of Product'];
+        obj.technology = item['Technologies'];
+        obj.year = parseInt(item['Certified in']);
+        obj.content = item['Excerpt'].replace(regEx, replaceMask).trim();
+
+        if (item['Category'] === 'Powered by FIWARE') {
+          poweredBy.push(obj);
+        } else if (item['Category'] === 'FIWARE-Ready') {
+          ready.push(obj);
+        } else if (item['Category'] === 'Services') {
+          service.push(obj);
+        } else if (item['Category'] === 'Cities4Cities') {
+          cities.push(obj);
+        } else {
+          console.log(item['Category']);
+        }
       }
     });
-    item.Domains = domains;
 
-    item.Technologies.split(',').forEach(item => {
-      if (item.trim() !== '') {
-        tech.push(item.trim());
+    // Products
+
+    fs.writeFile(
+      'poweredbyFiware/pageData.js',
+      'var pageData = ' + JSON.stringify(poweredBy, null, 2) + ';',
+      function(err) {
+        if (err) return console.log(err);
       }
-    });
-    item.Technologies = tech;
+    );
 
-    if (item.Member === 'False') {
-      item.Member = false;
-    }
-    if (item.Member === 'True') {
-      item.Member = true;
-    }
-    if (item.iHub === 'False') {
-      item.iHub = false;
-    }
-    if (item.iHub === 'True') {
-      item.iHub = true;
-    }
+    // Devices
 
-    const obj = {};
-    obj.company = item['Organisation Name'];
-    obj.name = item['Product Name'];
-    obj.img = item['Logo'];
-    obj.fiwareMember = item.Member;
-    obj.fiwareIhub = item.iHub;
-    obj.companyLink = item['Product Website'];
-    obj.domain = item['Domains'];
-    obj.type = item['Type of Product'];
-    obj.technology = item['Technologies'];
-    obj.year = item['Certified in'];
-    obj.content = item['Excerpt'].replace(regEx, replaceMask);
+    fs.writeFile(
+      'fiwareready/pageData.js',
+      'var pageData = ' + JSON.stringify(poweredBy, null, 2) + ';',
+      function(err) {
+        if (err) return console.log(err);
+      }
+    );
 
-    if (item['Category'] === 'Powered by FIWARE') {
-      poweredBy.push(obj);
-    } else if (item['Category'] === 'FIWARE-Ready') {
-      ready.push(obj);
-    } else if (item['Category'] === 'Services') {
-      service.push(obj);
-    } else if (item['Category'] === 'Cities4Cities') {
-      cities.push(obj);
-    } else {
-      console.log(item['Category']);
-    }
-  }
-});
+    // Services
 
-// Products
-fs.writeFile('powered.json', JSON.stringify(poweredBy, null, 2), function(err) {
-  if (err) return console.log(err);
-});
+    fs.writeFile(
+      'services/pageData.js',
+      'var pageData = ' + JSON.stringify(service, null, 2) + ';',
+      function(err) {
+        if (err) return console.log(err);
+      }
+    );
 
-fs.writeFile(
-  'poweredbyFiware/pageData.js',
-  'var pageData = ' + JSON.stringify(poweredBy, null, 2) + ';',
-  function(err) {
-    if (err) return console.log(err);
-  }
-);
+    // Cities
 
-// Devices
-fs.writeFile('ready.json', JSON.stringify(ready, null, 2), function(err) {
-  if (err) return console.log(err);
-});
-
-fs.writeFile(
-  'fiwareready/pageData.js',
-  'var pageData = ' + JSON.stringify(poweredBy, null, 2) + ';',
-  function(err) {
-    if (err) return console.log(err);
-  }
-);
-
-// Services
-fs.writeFile('service.json', JSON.stringify(service, null, 2), function(err) {
-  if (err) return console.log(err);
-});
-
-fs.writeFile(
-  'services/pageData.js',
-  'var pageData = ' + JSON.stringify(service, null, 2) + ';',
-  function(err) {
-    if (err) return console.log(err);
-  }
-);
-
-// Cities
-fs.writeFile('cities.json', JSON.stringify(cities, null, 2), function(err) {
-  if (err) return console.log(err);
-});
-
-fs.writeFile(
-  'cities4cities/pageData.js',
-  'var pageData = ' + JSON.stringify(cities, null, 2) + ';',
-  function(err) {
-    if (err) return console.log(err);
-  }
-);
+    fs.writeFile(
+      'cities4cities/pageData.js',
+      'var pageData = ' + JSON.stringify(cities, null, 2) + ';',
+      function(err) {
+        if (err) return console.log(err);
+      }
+    );
+  });
