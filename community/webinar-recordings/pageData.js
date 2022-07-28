391d7e52ffeedcaf7cf9fd4125806a01380f7b7f
Jason Fox <jason.fox@fiware.org>
2022-07-28 13:16:41 +0200

    Add file

---
+++ community/webinar-recordings/pageData.js
@@ -0,0 +1,14 @@
+var pageData = [
+  {
+    name: 'Introduction to FIWARE',
+    img: 'https://fiware.github.io/academy/img/intro.png',
+    companyLink: "https://youtu.be/97JsnnpPLrA'",
+    domain: ['Technology', 'Business', 'Community', 'iHubs'],
+    type: 'Fundamentals',
+    technology: ['NGSI-v2'],
+    year: 2017,
+    difficulty: 0,
+    content:
+      '<ul>\n<li>What is context data?</li>\n<li>How is context data used within a Smart Solution?</li>\n<li>What is FIWARE? How does it help</li>\n<li>What is NGSI v2?</li>\n<li>What is the FIWARE Catalogue?</li>\n<li>What is the FIWARE Marketplace?</li>\n<li>What is the role of the FIWARE Foundation?</li>\n</ul>\n<p>This video presentation is a basic introduction describing what FIWARE is, why you need it and how the elements of the FIWARE Catalogue can help accelerate the development of your Smart Solution.</p>'
+  }
+];
