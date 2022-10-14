db1779ba29a3a9dcca41b3baa66c32eed2d7c119
GitHub Action <action@github.com>
2022-10-14 12:42:06 +0200

    Tidying

--- app_featured.js
+++ app_featured.js
@@ -19,15 +19,19 @@ function includeHTML() {
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }      
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /*exit the function:*/
@@ -37,46 +41,47 @@ function includeHTML() {
 }
 
 function waitFor(conditionFunction) {
-  const poll = resolve => {
-    if(conditionFunction()) resolve();
-    else setTimeout(_ => poll(resolve), 400);
-  }
+  const poll = (resolve) => {
+    if (conditionFunction()) resolve();
+    else setTimeout((_) => poll(resolve), 400);
+  };
   return new Promise(poll);
 }
 
-function enableCarousel(){
-
-  console.log(jQuery('.owl-carousel').children().length)
-  jQuery('.owl-carousel').owlCarousel({
-      stagePadding:30,
-      loop:false,
-      margin:15,
-      nav:true,
-      navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          },
-          1400:{
-              items:4
-          },
-          1600:{
-              items:4
-          }
-      }
-  })
+function enableCarousel() {
+  waitFor((_) => jQuery(".owl-carousel").children().length > 13).then((_) =>
+    jQuery(".owl-carousel").owlCarousel({
+      stagePadding: 30,
+      loop: false,
+      margin: 15,
+      nav: true,
+      navText: [
+        '<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
+        '<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+        1400: {
+          items: 4,
+        },
+        1600: {
+          items: 4,
+        },
+      },
+    })
+  );
 }
 
-defer(async function () {
+defer(function () {
   // POPULATE THE LISTING
-  await includeHTML();
-  waitFor(_ => jQuery('.owl-carousel').children().length > 10)
-  .then(_ => enableCarousel()); 	
+  includeHTML();
+  enableCarousel();
 });
-
