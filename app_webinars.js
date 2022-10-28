ce4abee03d605a62c347ddac898848373a117287
GitHub Action <action@github.com>
2022-10-28 12:27:30 +0200

    Use w3 lib function with callback

--- app_webinars.js
+++ app_webinars.js
@@ -8,34 +8,6 @@ function defer(method) {
   }
 }
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
-
 function initDropdowns () {
  
   // update Technology Select
@@ -181,10 +153,8 @@ function concatValues(obj) {
   return value;
 }
 
-defer(function () {
 
-  // POPULATE THE LISTING
-  includeHTML();
+function enableListings(){
   // POPULATE THE INITIAL SELECT
   initDropdowns();
 
@@ -385,4 +355,15 @@ defer(function () {
       }
     });
   });
-});
+}
+
+
+
+defer(function () {
+  if (window.jQuery){
+    // POPULATE THE LISTING
+    w3.includeHTML(()=> {
+      enableListings();
+    });
+  }
+})
