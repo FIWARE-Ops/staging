ddd44a94634c32c0bb9842304228e302be4960f3
GitHub Action <action@github.com>
2022-10-28 12:22:15 +0200

    Use w3 lib function with callback

--- app_featured.js
+++ app_featured.js
@@ -8,82 +8,43 @@ function defer(method) {
   }
 }
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
 
-function waitFor(conditionFunction) {
-  const poll = (resolve) => {
-    if (conditionFunction()) resolve();
-    else setTimeout((_) => poll(resolve), 400);
-  };
-  return new Promise(poll);
-}
 
 function enableCarousel() {
-  waitFor((_) => jQuery(".owl-carousel").children().length > 13).then((_) =>
-    Promise.all(Array.from(jQuery(".owl-carousel img")).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-      jQuery(".owl-carousel").owlCarousel({
-        stagePadding: 30,
-        loop: false,
-        margin: 15,
-        nav: true,
-        navText: [
-          '<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
-          '<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>',
-        ],
-        responsive: {
-          0: {
-            items: 1,
-          },
-          600: {
-            items: 2,
-          },
-          1000: {
-            items: 3,
-          },
-          1400: {
-            items: 4,
-          },
-          1600: {
-            items: 4,
-          },
-        },
-      })
-    })
-  );
+  jQuery(".owl-carousel").owlCarousel({
+    stagePadding: 30,
+    loop: false,
+    margin: 15,
+    nav: true,
+    navText: [
+      '<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
+      '<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1400: {
+        items: 4,
+      },
+      1600: {
+        items: 4,
+      },
+    },
+  })
 }
 
 defer(function () {
-  // POPULATE THE LISTING
-  includeHTML();
-  enableCarousel();
-});
+  if (window.jQuery){
+    // POPULATE THE LISTING
+    w3.includeHTML(()=> {
+      enableCarousel();
+    });
+  }
+})
