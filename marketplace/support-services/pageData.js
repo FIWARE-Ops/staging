b4aa918f0b5f93f08ee848ac4c6f8fb77da807c5
GitHub Action <action@github.com>
2022-11-10 11:04:13 +0000

    Update Staging

--- marketplace/support-services/pageData.js
+++ marketplace/support-services/pageData.js
@@ -544,7 +544,8 @@ var pageData = [
       'https://www.fiware.org/style/imgs/Marketplace/product/services/consultancy-and-integration/logo_TeamDev.svg',
     fiwareMember: true,
     fiwareIhub: false,
-    companyLink: 'https://www.teamdev.it/en/',
+    companyLink:
+      '../product-details/?category=services&id=teamdev-fiwareprofessionalservices',
     domain: [
       'Cities',
       'Government',
