3ae508b6e16a314efd3f0eb1b6193455d2bf161f
GitHub Action <action@github.com>
2022-12-22 17:32:30 +0100

    Filter dropdowns

--- app_people.js
+++ app_people.js
@@ -16,24 +16,28 @@ function includeHTML(cb) {
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           elmnt.removeAttribute("w3-include-html");
           includeHTML(cb);
         }
-      }      
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       return;
     }
   }
   if (cb) cb();
-};
+}
 
-function addOptions(id, allText , data){
-  if (document.querySelector(id) && data ) {
+function addOptions(id, allText, data) {
+  if (document.querySelector(id) && data) {
     var companiesTmpl = `<option value='*'>${allText}</option>`;
     data.forEach((el) => {
       var techClass = createClassFilter(el);
@@ -41,18 +45,17 @@ function addOptions(id, allText , data){
       companiesTmpl += selectEl;
     });
     document.querySelector(id).innerHTML = companiesTmpl;
-  } 
+  }
 }
 
-function initDropdowns () {
-    addOptions("#filterCompany", 'All organizations', window.companies);
-    addOptions("#filterRole", 'All roles', window.titles);
-    addOptions("#filterDepartment", 'All departments', window.departments);
-    addOptions("#filterDomain", 'All domains', window.domains);
-    addOptions("#filterCountry", 'All countries', window.countries);
+function initDropdowns() {
+  addOptions("#filterCompany", "All organizations", window.companies);
+  addOptions("#filterRole", "All roles", window.titles);
+  addOptions("#filterDepartment", "All departments", window.departments);
+  addOptions("#filterDomain", "All domains", window.domains);
+  addOptions("#filterCountry", "All countries", window.countries);
 }
 
-  
 // Returns the right classNames for isotope card filtering system
 function createClassFilter(data) {
   var filterString = "";
@@ -72,7 +75,7 @@ function createClassFilter(data) {
   return filterString;
 }
 
-function inputSearch (itemElem, textString) {
+function inputSearch(itemElem, textString) {
   var stopwords = /\b(FIWARE|IoT|Smart|Solution|Product|Device)\b/gi;
   var words = textString.trim().replaceAll(stopwords, "").split(/[ ,]+/);
   var regex = [];
@@ -93,30 +96,33 @@ function concatValues(obj) {
   return value;
 }
 
-function initTextSearch(msnry){
-   // Search input
-    document.querySelector("#searchInput").addEventListener("keyup", (e) => {
-      if (e.target.value != "") {
-        e.target.parentNode.classList.add("resetActive");
-      } else {
-        e.target.parentNode.classList.remove("resetActive");
-      }
-      msnry.arrange({
-        filter: function (itemElem, itemElem2) {
-          return inputSearch(itemElem2, e.target.value);
-        },
-      });
+function initTextSearch(msnry) {
+  // Search input
+  document.querySelector("#searchInput").addEventListener("keyup", (e) => {
+    if (e.target.value != "") {
+      e.target.parentNode.classList.add("resetActive");
+    } else {
+      e.target.parentNode.classList.remove("resetActive");
+    }
+    msnry.arrange({
+      filter: function (itemElem, itemElem2) {
+        return inputSearch(itemElem2, e.target.value);
+      },
     });
+  });
 }
 
-function filterToggle(){
-   let filtersContainer = document.querySelector(".filters-container");
-    document.querySelector("#mobileToggleFilters").addEventListener("click", (ev) => {
+function filterToggle() {
+  let filtersContainer = document.querySelector(".filters-container");
+  document
+    .querySelector("#mobileToggleFilters")
+    .addEventListener("click", (ev) => {
       ev.target.classList.toggle("activeButton");
 
       if (!filtersContainer.classList.contains("active")) {
         filtersContainer.classList.add("active");
-        document.querySelector("#filter-button-text").innerText = "Hide Filters";
+        document.querySelector("#filter-button-text").innerText =
+          "Hide Filters";
         filtersContainer.style.height = "auto";
 
         let height = filtersContainer.clientHeight + "px";
@@ -128,7 +134,8 @@ function filterToggle(){
         }, 0);
       } else {
         filtersContainer.style.height = "0px";
-        document.querySelector("#filter-button-text").innerText = "Show Filters";
+        document.querySelector("#filter-button-text").innerText =
+          "Show Filters";
 
         filtersContainer.addEventListener(
           "transitionend",
@@ -143,59 +150,127 @@ function filterToggle(){
     });
 }
 
+function getCSSFilter(id) {
+  var cssFilter = "";
+  var currentType = $(id).val();
+  if (currentType !== "*") {
+    cssFilter = "." + currentType;
+  }
+  return document.querySelector(id) ? cssFilter : "";
+}
+
+function filterOptions(id, filter, data, css) {
+  // update Type select
+  if (document.querySelector(id) && data && filter) {
+    var arr = ["*"];
+    data.forEach((el) => {
+      var typeClass = createClassFilter(el);
+      if (typeClass !== "" && $("." + typeClass + css).size()) {
+        arr.push(typeClass);
+      }
+    });
+    $(`${id} option`).each(function () {
+      if (arr.includes($(this).val())) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+}
+
+function dropdownFilters(filter) {
+  var itemCSSFilter = ".grid-item:visible";
+
+  var companyCSSFilter = getCSSFilter("#filterCompany");
+  var roleCSSFilter = getCSSFilter("#filterRole");
+  var departmentCSSFilter = getCSSFilter("#filterDepartment");
+  var domainCSSFilter = getCSSFilter("#filterDomain");
+  var countryCSSFilter = getCSSFilter("#filterCountry");
+
+  filterOptions(
+    "#filterCompany",
+    filter.fCompany,
+    window.companies,
+    roleCSSFilter + departmentCSSFilter + domainCSSFilter + countryCSSFilter
+  );
+  filterOptions(
+    "#filterRole",
+    "All roles",
+    window.titles,
+    companyCSSFilter + departmentCSSFilter + domainCSSFilter + countryCSSFilter
+  );
+  filterOptions(
+    "#filterDepartment",
+    "All departments",
+    window.departments,
+    companyCSSFilter + roleCSSFilter + domainCSSFilter + countryCSSFilter
+  );
+  filterOptions(
+    "#filterDomain",
+    "All domains",
+    window.domains,
+    companyCSSFilter + roleCSSFilter + departmentCSSFilter + countryCSSFilter
+  );
+  filterOptions(
+    "#filterCountry",
+    "All countries",
+    window.countries,
+    companyCSSFilter + roleCSSFilter + departmentCSSFilter + domainCSSFilter
+  );
+}
 
 var init = false;
 
-
-
 function initSelect() {
-  if (init){return;}
+  if (init) {
+    return;
+  }
   init = true;
 
-    // POPULATE THE INITIAL SELECT
-   initDropdowns();
+  // POPULATE THE INITIAL SELECT
+  initDropdowns();
 
-    // Isotope istantiation
-    
-    var msnry;
-    var selectors = { 
-      fCompany: true,
-      fRole: true,
-      fDepartment: true,
-      fDomain: true,
-      fCountry: true};
-    var filterObj = {};
+  // Isotope istantiation
 
+  var msnry;
+  var selectors = {
+    fCompany: true,
+    fRole: true,
+    fDepartment: true,
+    fDomain: true,
+    fCountry: true,
+  };
+  var filterObj = {};
 
-    // Relies on unpkg.com/imagesloaded
-    imagesLoaded(document.querySelector("#app"), function (instance) {
-      msnry = new Isotope(".grid", {
-        itemSelector: ".grid-item",
-        layoutMode: "fitRows",
-        masonry: {
-          columnWidth: ".grid-sizer",
-        },
-        getSortData: {
-          name: ".name parseInt",
-          year: ".year",
-        },
-        sortAscending: {
-          name: true,
-          year: false
-        }
-      });
-
-      initTextSearch(msnry);
-      msnry.on("arrangeComplete", (filteredItems) => {
-        if (document.activeElement !== document.getElementById("searchInput")) {
-          $("html, body").scrollTop($("#searchInput").offset().top + 70);
-        }
-        dropdownFilters(selectors);
-      });
+  // Relies on unpkg.com/imagesloaded
+  imagesLoaded(document.querySelector("#app"), function (instance) {
+    msnry = new Isotope(".grid", {
+      itemSelector: ".grid-item",
+      layoutMode: "fitRows",
+      masonry: {
+        columnWidth: ".grid-sizer",
+      },
+      getSortData: {
+        name: ".name parseInt",
+        year: ".year",
+      },
+      sortAscending: {
+        name: true,
+        year: false,
+      },
     });
 
-    filterToggle(); 
-   
+    initTextSearch(msnry);
+    msnry.on("arrangeComplete", (filteredItems) => {
+      if (document.activeElement !== document.getElementById("searchInput")) {
+        $("html, body").scrollTop($("#searchInput").offset().top + 70);
+      }
+      dropdownFilters(selectors);
+    });
+  });
+
+  filterToggle();
 
   /*
     document.querySelector(".resetInput").addEventListener("click", (el) => {
@@ -230,96 +305,19 @@ function initSelect() {
       }
     });*/
 
-
-
-function dropdownFilters (filter)  {
-
-  /*
-  var itemCSSFilter = ".grid-item:visible";
-  var typeCSSFilter = "";
-  var currentType = jQuery("#filterType").val();
-  if (currentType !== "*") {
-    typeCSSFilter = "." + currentType;
-  }
-
-  var domainCSSFilter = "";
-  var currentDomain = jQuery("#filterDomain").val();
-  if (currentDomain !== "*") {
-    domainCSSFilter = "." + currentDomain;
-  }
-
-  var techCSSFilter = "";
-  var currentTech = jQuery("#filterTechnology").val();
-  if (currentTech !== "*") {
-    techCSSFilter = "." + currentTech;
-  }
-
-  // update Type select
-  if (window.types && filter.fType) {
-    var typesArr = ["*"];
-    window.types.forEach((el) => {
-      var typeClass = createClassFilter(el);
-      if (typeClass !== '' && jQuery("." + typeClass + domainCSSFilter + techCSSFilter + itemCSSFilter).size()) {
-        typesArr.push(typeClass);
-      }
-    });
-    $("#filterType option").each(function () {
-      if (typesArr.includes($(this).val())) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  }
-
-  // update Domain select
-  if (window.domains && filter.fDomain) {
-    var companyDomainArr = ["*"];
-    window.domains.forEach((el) => {
-      var domainClass = createClassFilter(el);
-      if (domainClass !== '' && jQuery("." + domainClass + typeCSSFilter + techCSSFilter + itemCSSFilter).size()) {
-        companyDomainArr.push(domainClass);
-      }
-    });
-    $("#filterDomain option").each(function () {
-      if (companyDomainArr.includes($(this).val())) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  }
-
-  // update Technology Select
-  if (window.technologies &&  filter.fTech) {
-    var companiesTechnologyArr = ["*"];
-    window.technologies.forEach((el) => {
-      var techClass = createClassFilter(el);
-      if (techClass !== '' && jQuery("." + techClass + typeCSSFilter + domainCSSFilter + itemCSSFilter).size()) {
-        companiesTechnologyArr.push(techClass);
-      }
-    });
-    $("#filterTechnology option").each(function () {
-      if (companiesTechnologyArr.includes($(this).val())) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  }*/
-}
-
-    document.querySelector(".filters-container").addEventListener("change", (e) => {
+  document
+    .querySelector(".filters-container")
+    .addEventListener("change", (e) => {
       if (e.target.id === "searchInput") {
         return;
       }
 
       selectors = {
-          fCompany: e.target.id !== "filterCompany",
-          fRole: e.target.id !== "filterRole",
-          fDepartment: e.target.id !== "filterDepartment",
-          fDomain: e.target.id !== "filterDomain",
-          fCountry: e.target.id !== "filterCountry",
+        fCompany: e.target.id !== "filterCompany",
+        fRole: e.target.id !== "filterRole",
+        fDepartment: e.target.id !== "filterDepartment",
+        fDomain: e.target.id !== "filterDomain",
+        fCountry: e.target.id !== "filterCountry",
       };
 
       if (document.getElementById(e.target.id).value === "*") {
@@ -328,21 +326,20 @@ function dropdownFilters (filter)  {
           fRole: true,
           fDepartment: true,
           fDomain: true,
-          fCountry: true
+          fCountry: true,
         };
       }
 
-      filterObj[e.target.id] = `${e.target.value == "*" ? "" : "." + e.target.value}`;
+      filterObj[e.target.id] = `${
+        e.target.value == "*" ? "" : "." + e.target.value
+      }`;
       msnry.arrange({
         filter: concatValues(filterObj),
       });
     });
+}
 
-   
-}    
-
-function smoothScroll () { 
-
+function smoothScroll() {
   // Add smooth scrolling to all links
   jQuery("a").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -367,54 +364,53 @@ function smoothScroll () {
   });
 }
 
-
-function horizontalScroll () { 
-    // Horizontal Scroll
-    var sliders = document.querySelectorAll(".chips");
-    var isDown = false;
-    var startX;
-    var scrollLeft;
-    sliders.forEach(function (slider) {
-      slider.addEventListener("mousedown", function (e) {
-        isDown = true;
-        slider.classList.add("active");
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-      });
-      slider.addEventListener("mouseleave", function () {
-        isDown = false;
-        slider.classList.remove("active");
-      });
-      slider.addEventListener("mouseup", function () {
-        isDown = false;
-        slider.classList.remove("active");
-      });
-      slider.addEventListener("mousemove", function (e) {
-        if (!isDown) return;
-        e.preventDefault();
-        var x = e.pageX - slider.offsetLeft;
-        var walk = (x - startX) * 3; //scroll-fast
-
-        slider.scrollLeft = scrollLeft - walk;
-        var links = slider.querySelectorAll(".item");
-
-        for (var i = 0; i < links.length; i++) {
-          links[i].classList.add("noclick");
-        }
-      });
+function horizontalScroll() {
+  // Horizontal Scroll
+  var sliders = document.querySelectorAll(".chips");
+  var isDown = false;
+  var startX;
+  var scrollLeft;
+  sliders.forEach(function (slider) {
+    slider.addEventListener("mousedown", function (e) {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
     });
+    slider.addEventListener("mouseleave", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mouseup", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mousemove", function (e) {
+      if (!isDown) return;
+      e.preventDefault();
+      var x = e.pageX - slider.offsetLeft;
+      var walk = (x - startX) * 3; //scroll-fast
+
+      slider.scrollLeft = scrollLeft - walk;
+      var links = slider.querySelectorAll(".item");
+
+      for (var i = 0; i < links.length; i++) {
+        links[i].classList.add("noclick");
+      }
+    });
+  });
 }
 
 defer(function () {
   // POPULATE THE LISTING
-  jQuery(document).ready(function() {
+  jQuery(document).ready(function () {
     jQuery("#app").css("visibility", "hidden");
-    includeHTML(function() {
-      initSelect();
+    includeHTML(function () {
       horizontalScroll();
       smoothScroll();
-       $('#app').waitForImages(function() {
-         $("#app").css("visibility", "visible");
+      $("#app").waitForImages(function () {
+        initSelect();
+        $("#app").css("visibility", "visible");
       });
     });
   });
