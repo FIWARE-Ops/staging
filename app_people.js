316c816eb18dca81bdd2148c5a997e12af2ad0ac
GitHub Action <action@github.com>
2022-12-22 18:05:52 +0100

    Fix modal.

--- app_people.js
+++ app_people.js
@@ -181,8 +181,6 @@ function filterOptions(id, filter, data, css) {
 }
 
 function dropdownFilters(filter) {
-  
-
   var companyCSSFilter = getCSSFilter("#filterCompany");
   var roleCSSFilter = getCSSFilter("#filterRole");
   var departmentCSSFilter = getCSSFilter("#filterDepartment");
@@ -219,6 +217,105 @@ function dropdownFilters(filter) {
     window.countries,
     companyCSSFilter + roleCSSFilter + departmentCSSFilter + domainCSSFilter
   );
+}
+
+function createModalContent(tingleModalData) {
+  var modalHtml = "";
+  console.warn(tingleModalData);
+
+  modalHtml += "<div class='info-modal'>";
+  modalHtml += '<img src="' + tingleModalData.img + '" />';
+  modalHtml += "<div class='credits-modal'>";
+  if (tingleModalData.name != "null") {
+    modalHtml += "<h1>" + tingleModalData.name + "</h1>";
+  }
+  if (tingleModalData.position != "null") {
+    modalHtml += "<h2>" + tingleModalData.position + "</h2>";
+  }
+  if (tingleModalData.company != "null") {
+    modalHtml +=
+      '<a class="company-link" href="' +
+      tingleModalData["company-link"] +
+      '" target="_blank">' +
+      tingleModalData.company +
+      "</a>";
+  }
+  modalHtml += "</div>";
+  modalHtml += "</div>";
+  modalHtml += "<div class='bio-modal'>";
+  if (tingleModalData.content != "null") {
+    modalHtml += "<p>" + tingleModalData.content + "</p>";
+  }
+  modalHtml += "</div>";
+  modalHtml += "<div class='details-modal'>";
+  modalHtml += "<div class='social-modal'>";
+
+  if (tingleModalData.twitter != null) {
+    modalHtml +=
+      '<a class="twitter-link" href="' +
+      tingleModalData["twitter"] +
+      '" target="_blank"></a>';
+  }
+  if (tingleModalData.linkedin != null) {
+    modalHtml +=
+      '<a class="linkedin-link" href="' +
+      tingleModalData["linkedin"] +
+      '" target="_blank"></a>';
+  }
+  if (tingleModalData.domain != "null") {
+    modalHtml += '<p class="domain">' + tingleModalData.domain + "</p>";
+  }
+  modalHtml += "</div>";
+  modalHtml += "<div class='tags-modal'>";
+  if (tingleModalData.location != "null") {
+    modalHtml += '<p class="location">' + tingleModalData.location + "</p>";
+  }
+
+  modalHtml += "</div>";
+
+  return modalHtml;
+}
+
+function initModal() {
+  // Modal
+  document.querySelectorAll(".cat-bio").forEach(function (el) {
+    el.addEventListener("click", function (e) {
+      var modal = new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeMethods: ["overlay", "button", "escape"],
+        closeLabel: "Close",
+        cssClass: ["custom-class-1", "custom-class-2"],
+        onOpen: function () {
+          console.log("modal open");
+        },
+        onClose: function () {
+          console.log("modal closed");
+        },
+        beforeClose: function () {
+          // here's goes some logic
+          // e.g. save content before closing the modal
+          return true; // close the modal
+          return false; // nothing happens
+        },
+      });
+      // set content
+
+      console.log(el);
+      console.log(el.dataset.modal);
+
+      modal.setContent(createModalContent(window.modalData[el.dataset.modal]));
+
+      // open modal
+      modal.open();
+    });
+  });
+
+  $(document).ready(function () {
+    $(".f-cat a").on("click", function (e) {
+      e.stopPropagation();
+    });
+  });
 }
 
 var init = false;
@@ -407,11 +504,12 @@ defer(function () {
   jQuery(document).ready(function () {
     jQuery("#app").css("visibility", "hidden");
     includeHTML(function () {
-      horizontalScroll();
-      smoothScroll();
       $("#app").waitForImages(function () {
         initSelect();
         $("#app").css("visibility", "visible");
+        initModal();
+        horizontalScroll();
+        smoothScroll();
       });
     });
   });
