bf12d3aa65988d1c2e16b602ac96a744bf46be5c
GitHub Action <action@github.com>
2023-01-11 15:17:05 +0100

    Sort order on teams

--- bin/labels.js
+++ bin/labels.js
@@ -14,32 +14,36 @@ const DEFAULT_LABELS = {
   allRoles: "All Roles",
   allDomains: "All Domains",
   allCommittees: "All Committees",
-  allTypes: "All Speaker Type",
+  allTypes: "All Speaker Types",
 };
 
 // Base data - add one for each page
 const labels = {
   bod: _.clone(DEFAULT_LABELS),
+  boo: _.clone(DEFAULT_LABELS),
+  experts: _.clone(DEFAULT_LABELS),
+  evangelists: _.clone(DEFAULT_LABELS),
   fisab: _.clone(DEFAULT_LABELS),
   msc: _.clone(DEFAULT_LABELS),
   team: _.clone(DEFAULT_LABELS),
-  speakers: _.clone(DEFAULT_LABELS),
-  experts: _.clone(DEFAULT_LABELS),
-  evangelists: _.clone(DEFAULT_LABELS),
+  tsc: _.clone(DEFAULT_LABELS),
+  speakers: _.clone(DEFAULT_LABELS)
 };
 
 // Page specific titles - add one for each page
-labels.fisab.title = 'Scientific Advisory Board';
 labels.bod.title = 'Board of Directors';
-labels.msc.title = 'Mission Support Committee';
+labels.boo.title = 'Board of Officers';
+labels.fisab.title = 'Scientific Advisory Board';
+labels.experts.title = 'Experts';
+labels.evangelists.title = 'Evangelists';
+labels.msc.title = 'Mission Support Committees';
 labels.team.title = 'FIWARE Team';
+labels.tsc.title = 'Technical Steering Committee';
 labels.speakers.title = 'Speakers';
-labels.experts.title = "Experts";
-labels.evangelists.title = "Evangelists";
 
 //  Specific Label overrides for FIWARE Team
-labels.team.countries = 'Location';
-labels.team.allCountries = 'All Locations';
+// labels.team.countries = 'Working Location';
+// labels.team.allCountries = 'All Locations';
 
 function getLabels(type) {
   return labels[type];
