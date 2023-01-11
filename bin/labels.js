4c21dc22da3c217c2566520fee1b3f91aaec7b47
Giacomo <reddy.depa@gmail.com>
2023-01-11 15:03:55 +0100

    label.js content update

--- bin/labels.js
+++ bin/labels.js
@@ -1,38 +1,44 @@
 const _ = require('underscore');
 
 const DEFAULT_LABELS = {
-  departments: 'Department',
-  countries: 'Countries',
-  roles: 'Role',
-  domains: 'Domain',
-  allDepartments: 'All Departments',
-  allCountries: 'All Locations',
-  allRoles: 'All Roles',
-  allDomains: 'All Domains'
+  companies: "Company",
+  departments: "Department",
+  countries: "Countries",
+  roles: "Role",
+  domains: "Domain",
+  committees: "Committee",
+  types: "Speaker Type",
+  allCompanies: "All Companies",
+  allDepartments: "All Departments",
+  allCountries: "All Locations",
+  allRoles: "All Roles",
+  allDomains: "All Domains",
+  allCommittees: "All Committees",
+  allTypes: "All Speaker Type",
 };
 
 // Base data - add one for each page
 const labels = {
   bod: _.clone(DEFAULT_LABELS),
-  boo: _.clone(DEFAULT_LABELS),
   fisab: _.clone(DEFAULT_LABELS),
   msc: _.clone(DEFAULT_LABELS),
   team: _.clone(DEFAULT_LABELS),
-  tsc: _.clone(DEFAULT_LABELS),
-  speakers: _.clone(DEFAULT_LABELS)
+  speakers: _.clone(DEFAULT_LABELS),
+  experts: _.clone(DEFAULT_LABELS),
+  evangelists: _.clone(DEFAULT_LABELS),
 };
 
 // Page specific titles - add one for each page
 labels.fisab.title = 'Scientific Advisory Board';
 labels.bod.title = 'Board of Directors';
-labels.boo.title = 'Board of Officers';
-labels.msc.title = 'Mission Support Committees';
+labels.msc.title = 'Mission Support Committee';
 labels.team.title = 'FIWARE Team';
-labels.tsc.title = 'Mission Support Committee';
 labels.speakers.title = 'Speakers';
+labels.experts.title = "Experts";
+labels.evangelists.title = "Evangelists";
 
 //  Specific Label overrides for FIWARE Team
-labels.team.countries = 'Working Location';
+labels.team.countries = 'Location';
 labels.team.allCountries = 'All Locations';
 
 function getLabels(type) {
