33dd3f6f4df5314b3cc360e39a3298380a11bbd6
GitHub Action <action@github.com>
2023-01-11 10:19:49 +0100

    Add FISAB labels

--- bin/labels.js
+++ bin/labels.js
@@ -1,16 +1,29 @@
-const labels = {
-  fiware: {
-    title: 'FIWARE Team',
-    departments: 'Department',
-    countries: 'Working Location',
-    roles: 'Role',
-    domains: 'Domain',
-    allDepartments: 'All Departments',
-    allCountries: 'All Locations',
-    allRoles: 'All Roles',
-    allDomains: 'All Domains'
-  }
+const _ = require('underscore');
+
+const DEFAULT_LABELS = {
+  departments: 'Department',
+  countries: 'Countries',
+  roles: 'Role',
+  domains: 'Domain',
+  allDepartments: 'All Departments',
+  allCountries: 'All Locations',
+  allRoles: 'All Roles',
+  allDomains: 'All Domains'
 };
+
+// Base data - add one for each page
+const labels = {
+  fiware: _.clone(DEFAULT_LABELS),
+  fisab: _.clone(DEFAULT_LABELS)
+};
+
+// Page specific titles - add one for each page
+labels.fiware.title = 'FIWARE Team';
+labels.fisab.title = 'FISAB Team';
+
+//  Specific Label overrides for FIWARE Team
+labels.fiware.countries = 'Working Location';
+labels.fiware.allCountries = 'All Locations';
 
 function getLabels(type) {
   return labels[type];
