c370e17516c855ae33950c3a1457bfaf6719722e
Jason Fox <jason-fox@users.noreply.github.com>
2023-06-26 18:29:48 +0200

    Add badges

--- bin/enablers/staticData.js
+++ bin/enablers/staticData.js
@@ -1,8 +1,17 @@
 function getBadge(type) {
     let badge;
     switch (type) {
+        case 'API Management':
+            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_APIManagement.svg'/>`;
+            break;
         case 'Core Context':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Core.svg'/>`;
+            break;
+        case 'Data Monetization':
+            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_DataMonetization.svg'/>`;
+            break;
+        case 'Data Publication':
+            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_DataPublication.svg'/>`;
             break;
         case 'Robotics':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Robotics.svg'/>`;
@@ -10,14 +19,8 @@ function getBadge(type) {
         case 'Security':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Security.svg'/>`;
             break;
-        case 'Data Monetization':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_DataMonetization.svg'/>`;
-            break;
         case 'IoT Agents':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_IOTAgents.svg'/>`;
-            break;
-        case 'Fundamentals':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Fundamentals.svg'/>`;
             break;
         case 'Operations':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Operations.svg'/>`;
@@ -29,7 +32,6 @@ function getBadge(type) {
         case 'Visualisation':
             badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Visualization.svg'/>`;
             break;
-        case 'Cross-Chapter':
         default:
             badge = `<b>${type}</b>`;
             break;
