261d83031830e192e5caf80b40c22ce083bf2a8c
Jason Fox <jason-fox@users.noreply.github.com>
2023-06-27 18:50:23 +0200

    Update JS and CSS

--- directories/app_enablers.js
+++ directories/app_enablers.js
@@ -327,6 +327,52 @@ function smoothScroll() {
   });
 }
 
+function initFeaturedCarousel() {
+  $(".featured-carousel").owlCarousel({
+    stagePadding: 0,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1,
+        autoplay: true,
+        nav: false,
+        dots: true,
+        loop: true,
+      },
+      600: {
+        items: 3,
+        autoplay: true,
+        nav: false,
+        dots: true,
+        loop: true,
+      },
+      1000: {
+        items: 4,
+        autoplay: true,
+        nav: false,
+        dots: false,
+        loop: true,
+      },
+      1400: {
+        items: 4,
+        autoplay: true,
+        nav: false,
+        dots: false,
+        loop: true,
+      },
+      1600: {
+        items: 4,
+        autoplay: true,
+        nav: false,
+        dots: false,
+        loop: true,
+      },
+    },
+  });
+}
+
 function horizontalScroll() {
   // Horizontal Scroll
   var sliders = document.querySelectorAll(".chips");
@@ -373,6 +419,7 @@ document.addEventListener("DOMContentLoaded", () => {
       smoothScroll();
       initSelect();
       initModal();
+      //initFeaturedCarousel();
       filterToggle();
       // Isotope istantiation
       // Relies on unpkg.com/imagesloaded
