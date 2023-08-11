585c093ace4515ea5ff42607aa9eb09dd94d3b5a
Giacomo <reddy.depa@gmail.com>
2023-08-11 16:33:22 +0200

    organisations content update

--- directories/organisations/pageData.js
+++ directories/organisations/pageData.js
@@ -1,5 +1,10 @@
 var companies = [];
 var departments = [];
-var titles = ['Associate', 'Gold', 'Gold SEU', 'Platinum'];
-var domains = [];
-var countries = [];
+var titles = [
+        'Associate',
+        'Gold',
+        'Gold SEU',
+        'Platinum',
+]; 
+var domains = []; 
+var countries = []; 
