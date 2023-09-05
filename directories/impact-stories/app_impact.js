d53470a38605f38abc0cfffe35a40c99a97c0d6f
Giacomo <reddy.depa@gmail.com>
2023-09-05 15:37:04 +0200

    app impact-stories update

--- directories/impact-stories/app_impact.js
+++ directories/impact-stories/app_impact.js
@@ -369,6 +369,28 @@ function horizontalScroll() {
   });
 }
 
+function initSticky() {
+  window.onscroll = onScrollHandler;
+
+  function onScrollHandler() {
+    const header = document.getElementById("filters");
+    const footer = document.getElementById("no-sticky");
+
+    if (
+      !!header &&
+      !!footer &&
+      window.pageYOffset > header.offsetTop &&
+      window.pageYOffset < footer.offsetTop
+    ) {
+      header.classList.add("stickybar");
+      header.classList.remove("not-stickybar");
+    } else {
+      header.classList.remove("stickybar");
+      header.classList.add("not-stickybar");
+    }
+  }
+}
+
 document.addEventListener("html-included", () => {
   //$("#filteredCompanies").text(window.modalData.length);
   horizontalScroll();
@@ -404,25 +426,3 @@ document.addEventListener("html-included", () => {
       }
     });
 });
-
-function initSticky() {
-  window.onscroll = onScrollHandler;
-
-  function onScrollHandler() {
-    const header = document.getElementById("filters");
-    const footer = document.getElementById("no-sticky");
-
-    if (
-      !!header &&
-      !!footer &&
-      window.pageYOffset > header.offsetTop &&
-      window.pageYOffset < footer.offsetTop
-    ) {
-      header.classList.add("stickybar");
-      header.classList.remove("not-stickybar");
-    } else {
-      header.classList.remove("stickybar");
-      header.classList.add("not-stickybar");
-    }
-  }
-}
