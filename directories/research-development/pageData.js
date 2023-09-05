e78870b84acb7af258c5d6991ad9949514b7416f
Jason Fox <jason-fox@users.noreply.github.com>
2023-09-05 08:11:14 +0200

    Add R&D

---
+++ directories/research-development/pageData.js
@@ -0,0 +1,31 @@
+var companies = [];
+var departments = [];
+var titles = [
+        '2023',
+        '2022',
+        '2021',
+        '2020'
+]; var domains = [
+        'blueconomy',
+        'egovernance',
+        'ehealth',
+        'robotics',
+        'smartagrifood',
+        'smartagritech',
+        'smartbuildings',
+        'smartcities',
+        'smartenergy',
+        'smartindustry',
+        'smartstreetlighting',
+        'smartlogistics',
+        'smartmobility',
+        'smartports',
+        'smartspaces',
+        'smarttourism',
+        'smartwater',
+        'sustainability'
+]; var countries = [
+        'ec',
+        'germany'
+]; 
+
