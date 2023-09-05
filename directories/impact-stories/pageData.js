476834f54122f32edb37eb8f613310330c2c7130
Giacomo <reddy.depa@gmail.com>
2023-09-05 10:17:26 +0200

    impact stories folder first commit

---
+++ directories/impact-stories/pageData.js
@@ -0,0 +1,34 @@
+var companies = [];
+var departments = [];
+var titles = [
+        '2023',
+        '2022',
+        '2021',
+        '2020'
+]; var domains = [
+        'blueconomy',
+        'egovernance',
+        'ehealth',
+        'robotics',
+        'smartagrifood',
+        'smartagritech',
+        'smartbuildings',
+        'smartcities',
+        'smartenergy',
+        'smartindustry',
+        'smartstreetlighting',
+        'smartlogistics',
+        'smartmobility',
+        'smartports',
+        'smartspaces',
+        'smarttourism',
+        'smartwater',
+        'sustainability'
+]; var countries = [
+        'english',
+        'french',
+        'german',
+        'italian',
+        'spanish'
+]; 
+
