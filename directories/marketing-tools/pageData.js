2ce72396e0a3446422c2efe9ecaee9101e184f5a
GitHub Action <action@github.com>
2023-09-08 11:27:10 +0000

    Update Staging

---
+++ directories/marketing-tools/pageData.js
@@ -0,0 +1,25 @@
+var companies = [];
+var departments = [];
+var titles = [
+        'Book',
+        'Booklet',
+        'Brand Guideline',
+        'Brochures',
+        'Deck',
+        'Flyer',
+        'Position Paper',
+        'Press Kit',
+        'Promotion',
+        'White Paper',
+]; var domains = [
+        'AgriFood',
+        'Cities',
+        'eHealth',
+        'Energy',
+        'FIWARE',
+        'Green',
+        'Industry',
+        'Mobility',
+        'Sustainability',
+]; var countries = [
+]; 
