ef13865d3b2fe8835b250d9341d948de62d316db
GitHub Action <action@github.com>
2023-09-11 08:56:14 +0000

    Update Staging

--- marketplace/cities4cities/pageData.js
+++ marketplace/cities4cities/pageData.js
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+var pageData = [{"company":"City of Vienna","name":"Smart Data Wien","img":"https://www.fiware.org/style/imgs/Marketplace/product/cities4cities/logo_CityOfVienna.svg","fiwareMember":true,"fiwareIhub":false,"companyLink":"../product-details/?category=cities&id=cityofvienna-smartdatawien","domain":["Cities","Government"],"type":"","technology":["Internet of Things","Mobility and Location","Open Data"],"year":2017,"content":"Urban Data Platform of the City of Vienna"}];
+=======
 var pageData = [
     {
         company: 'City of Vienna',
@@ -15,3 +18,4 @@ var pageData = [
 ];
 
 loadProducts();
+>>>>>>> f9bc83453e11edbe5cd62be92e81a983b0b26c76
