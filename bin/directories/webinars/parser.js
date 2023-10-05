c11674e8c70cb55697e183b75468c9dfe0a258d3
Jason Fox <jason-fox@users.noreply.github.com>
2023-10-05 19:24:24 +0200

    Generate Webinar Social media

--- bin/directories/webinars/parser.js
+++ bin/directories/webinars/parser.js
@@ -11,6 +11,14 @@ const WEBINARS_DIR = 'directories/webinars';
 
 const DEFAULT_IMAGE = 'https://www.fiware.org/wp-content/directories/webinars/images/webinar-default.png';
 
+
+
+function getExcerpt(item){
+    let text =  Parser.textOnly(item['Content']);
+    const next = text.indexOf(".", 40);
+    return text.substring(0, next +1)
+}
+
 /**
  * Take the human readable column names from the spreadsheet and create a
  * data object of webinars for later use
@@ -25,6 +33,7 @@ function extractWebinars(input) {
             companyLink: item['Video'],
             domain: Parser.splitStrings(item['Audience']),
             type: item['Type'],
+            excerpt: getExcerpt(item),
             badge: Static.getBadge(item['Type']),
             technology: Parser.splitStrings(item['Technology']),
             year: parseInt(item['Year']),
@@ -67,6 +76,30 @@ function parse(file) {
                 webinars
             };
 
+            Template.clean(WEBINARS_DIR);
+
+            Template.write(
+                path.join(WEBINARS_DIR, 'sitemap.html'),
+                path.join(TEMPLATE_PATH, 'sitemap-html.hbs'),
+                webinars
+            );
+            Template.write(
+                path.join(WEBINARS_DIR, 'sitemap.xml'),
+                path.join(TEMPLATE_PATH, 'sitemap-xml.hbs'),
+                webinars
+            );
+
+            webinars.forEach ((webinar, index) =>{
+
+                const filename = Template.createClass(webinar.name);
+                Template.write(
+                    path.join(WEBINARS_DIR, `${filename}.html`),
+                    path.join(TEMPLATE_PATH, 'social-media.hbs'),
+                webinar);
+                Prettier.format(path.join(WEBINARS_DIR, `${filename}.html`), { parser: 'html' });
+            });
+
+
             Template.write(path.join(WEBINARS_DIR, 'webinars.html'), path.join(TEMPLATE_PATH, 'card.hbs'), webinars);
             Template.write(path.join(WEBINARS_DIR, 'pageData.js'), path.join(TEMPLATE_PATH, 'modal.hbs'), filterData);
             Template.write(path.join(WEBINARS_DIR, 'filters.html'), path.join(TEMPLATE_PATH, 'filter.hbs'), filterData);
@@ -74,6 +107,8 @@ function parse(file) {
             Prettier.format(path.join(WEBINARS_DIR, 'webinars.html'), { parser: 'html' });
             Prettier.format(path.join(WEBINARS_DIR, 'pageData.js'), { parser: 'flow' });
             Prettier.format(path.join(WEBINARS_DIR, 'filters.html'), { parser: 'html' });
+            Prettier.format(path.join(WEBINARS_DIR, 'sitemap.html'), { parser: 'html' });
+     
         })
         .catch((e) => {
             console.log(e);
