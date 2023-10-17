f58871181e3f4ac1462836eed4df154a577d8f4b
Giacomo <reddy.depa@gmail.com>
2023-10-17 17:21:05 +0200

    static data webinars update

--- bin/directories/webinars/staticData.js
+++ bin/directories/webinars/staticData.js
@@ -2,32 +2,32 @@ function getBadge(type) {
     let badge;
     switch (type) {
         case 'Core Context':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Core.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/01/svg/core.svg'/>`;
             break;
         case 'Robotics':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Robotics.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/02/svg/robotics.svg'/>`;
             break;
         case 'Security':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Security.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/04/svg/security.svg'/>`;
             break;
         case 'Data Monetization':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_DataMonetization.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/04/svg/data-monetization.svg'/>`;
             break;
         case 'IoT Agents':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_IOTAgents.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/02/svg/iot-agents.svg'/>`;
             break;
         case 'Fundamentals':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Fundamentals.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/01/svg/fundamentals.svg'/>`;
             break;
         case 'Operations':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Operations.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/operations/svg/operations.svg'/>`;
             break;
         case 'Processing':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_ProcessingAnalysis.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/03/svg/processing-analysis.svg'/>`;
             break;
         case 'Visualization':
         case 'Visualisation':
-            badge = `<img src='https://www.fiware.org/style/imgs/Badges/Badge_FIWARE_Visualization.svg'/>`;
+            badge = `<img src='https://www.fiware.org/custom/brand-guide/img/badges/catalogue/03/svg/visualisation.svg'/>`;
             break;
         case 'Cross-Chapter':
         default:
