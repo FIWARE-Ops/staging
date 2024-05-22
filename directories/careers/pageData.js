2d6fce5831614398e0098f723919d734a861a48c
GitHub Action <action@github.com>
2024-05-22 07:40:43 +0000

    Update Staging

--- directories/careers/pageData.js
+++ directories/careers/pageData.js
@@ -2,8 +2,6 @@ var departments = [
         'Entry Level',
         'Mid Level',
         'Senior Level',
-        'Working Student',
 ]; var domains = [
-        'Marketing Team',
         'Tech Team',
 ];
