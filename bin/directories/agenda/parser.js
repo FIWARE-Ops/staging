310dff338a0e2c2198710efaf816f5cfcc3b5452
Jason Fox <jason-fox@users.noreply.github.com>
2024-07-01 16:34:04 +0200

    Updated and formatted.

--- bin/directories/agenda/parser.js
+++ bin/directories/agenda/parser.js
@@ -17,13 +17,8 @@ const DEFAULT_IMAGE = 'https://www.fiware.org/wp-content/directories/agenda/imag
  * data object of each project for later use
  */
 function extractAgenda(input, speakers) {
-
     const agenda = [];
     input.forEach((item) => {
-
-
-
-
         const event = {
             track: item['Track'],
             session: item['Session'],
@@ -37,35 +32,32 @@ function extractAgenda(input, speakers) {
             publish: Parser.boolean(item['Published'])
         };
 
-
         if (event.publish) {
-            
+            const names = _.uniq(
+                _.filter(
+                    [
+                        item['Speaker 1'],
+                        item['Speaker 2'],
+                        item['Speaker 3'],
+                        item['Speaker 4'],
+                        item['Speaker 5'],
+                        item['Speaker 6']
+                    ],
+                    function (name) {
+                        return name !== '';
+                    }
+                )
+            );
 
-           const names =
-             _.uniq(_.filter([
-                item['Speaker 1'],
-                item['Speaker 2'],
-                item['Speaker 3'],
-                item['Speaker 4'],
-                item['Speaker 5'],
-                item['Speaker 1']
-            ], function(name) {
-                return name !== ''
-            }));
-
-            event.speakers = _.map(names, 
-                function(name){
-                    const speaker =  _.findWhere(speakers,{name});
-                    return speaker
-                });
+            event.speakers = _.map(names, function (name) {
+                const speaker = _.findWhere(speakers, { name });
+                return !!speaker ? speaker : { name, img: People.DEFAULT_IMAGE };
+            });
 
             event.startTime = Parser.addTime(event.date, event.start);
             agenda.push(event);
         }
     });
-
-
-
 
     if (agenda.length === 0) {
         console.error('ERROR: No agenda uploaded.');
@@ -74,9 +66,8 @@ function extractAgenda(input, speakers) {
     console.log(agenda.length, ' agenda items generated.');
 
     return agenda.sort((a, b) => {
-         return (a.startTime > b.startTime.getTime());
+        return a.startTime > b.startTime.getTime();
     });
-
 }
 
 /**
@@ -87,63 +78,51 @@ function parse(agendaFile, speakersFile) {
     let agendaData = null;
     let speakers = null;
 
-
-
     csv()
         .fromFile(speakersFile)
         .then((input) => {
             speakers = People.extract(input);
-  
-            speakerNames =  _.map(speakers, 
-                function(speaker){
-                    return speaker.name
+
+            speakerNames = _.map(speakers, function (speaker) {
+                return speaker.name;
+            });
+
+            csv()
+                .fromFile(agendaFile)
+                .then((input) => {
+                    return extractAgenda(input, speakers);
+                })
+                .then((agenda) => {
+
+                    const filterData = {
+                        types: Sorter.sortData(agenda, 'track'),
+                        domains: Sorter.flatSortData(agenda, 'session'),
+                        technologies: speakerNames,
+                        agenda
+                    };
+
+                    Template.clean(path.join(AGENDA_DIR, '/agenda'));
+
+                    Template.write(path.join(AGENDA_DIR, 'agenda.html'), path.join(TEMPLATE_PATH, 'card.hbs'), agenda);
+                    Template.write(
+                        path.join(AGENDA_DIR, 'pageData.js'),
+                        path.join(TEMPLATE_PATH, 'modal.hbs'),
+                        filterData
+                    );
+                    Template.write(
+                        path.join(AGENDA_DIR, 'filters.html'),
+                        path.join(TEMPLATE_PATH, 'filter.hbs'),
+                        filterData
+                    );
+
+                    Prettier.format(path.join(AGENDA_DIR, 'agenda.html'), { parser: 'html' });
+                    Prettier.format(path.join(AGENDA_DIR, 'pageData.js'), { parser: 'flow' });
+                })
+                .catch((e) => {
+                    console.log(e);
+                    return;
                 });
-
-
-  
-    csv()
-        .fromFile(agendaFile)
-        .then((input) => {
-            return extractAgenda(input, speakers);
-        })
-        .then((agenda) => {
-
-
-            console.log(agenda[0])
-
-            const filterData = {
-                types: Sorter.sortData(agenda, 'track'),
-                domains: Sorter.flatSortData(agenda, 'session'),
-                technologies: speakerNames,
-                agenda
-            };
-
-            Template.clean(path.join(AGENDA_DIR, '/agenda'));
-
-            Template.write(
-                path.join(AGENDA_DIR, 'agenda.html'),
-                path.join(TEMPLATE_PATH, 'card.hbs'),
-                agenda
-            );
-            Template.write(
-                path.join(AGENDA_DIR, 'pageData.js'),
-                path.join(TEMPLATE_PATH, 'modal.hbs'),
-                filterData
-            );
-            Template.write(
-                path.join(AGENDA_DIR, 'filters.html'),
-                path.join(TEMPLATE_PATH, 'filter.hbs'),
-                filterData
-            );
-
-            Prettier.format(path.join(AGENDA_DIR, 'agenda.html'), { parser: 'html' });
-            Prettier.format(path.join(AGENDA_DIR, 'pageData.js'), { parser: 'flow' });
-        })
-        .catch((e) => {
-            console.log(e);
-            return;
         });
-   });
 }
 
 exports.parse = parse;
