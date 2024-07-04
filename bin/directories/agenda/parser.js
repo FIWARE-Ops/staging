b9e5a05a758dfea92652737de09ed2b5d24daf50
Jason Fox <jason-fox@users.noreply.github.com>
2024-07-04 07:42:29 +0200

    Amend social

--- bin/directories/agenda/parser.js
+++ bin/directories/agenda/parser.js
@@ -75,7 +75,7 @@ function extractAgenda(input, speakers, activeSpeakers, eventDates) {
             event.startTime = Parser.addTime(event.date, event.start);
             event.shortDate = event.date.toLocaleDateString(undefined, {month: "long", day: "numeric"});
             const filename= Template.createClass(event.title);
-            event.social = `https://www.fiware.org/fgs-${new Date().getFullYear()}/${filename}.html`
+            event.social = `/fgs-${new Date().getFullYear()}/${filename}.html`
             eventDates.push(event.shortDate)
             agenda.push(event);
         }
