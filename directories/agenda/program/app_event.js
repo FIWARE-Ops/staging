0f5b4b0386da9504d206d6e60ec3434fed09a88e
Giacomo <reddy.depa@gmail.com>
2024-07-11 11:14:37 +0200

    app_event js update

--- directories/agenda/program/app_event.js
+++ directories/agenda/program/app_event.js
@@ -205,7 +205,7 @@ function trackCSS(data) {
     case "Tech Training":
       result = "light-green";
       break;
-    case "Empowering co-creation of technology solutions":
+    case "Empowering co-creation of tech solutions":
       result = "light-green";
       break;
     case "Innovation with FIWARE":
