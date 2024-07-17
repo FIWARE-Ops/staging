fef68f20419bcbe68b10ada736f8bfbbf01f7294
GitHub Action <action@github.com>
2024-07-17 14:41:05 +0000

    Update Staging

---
+++ directories/sponsors/pageData.js
@@ -0,0 +1,13 @@
+var companies = [];
+var departments = [];
+var titles = [
+        'Basic Sponsor',
+        'Exhibitor',
+        'Hosting Partner',
+        'Media Partners',
+        'Official Partner',
+        'Premiere Sponsor',
+        'Prestige Sponsor',
+]; 
+var domains = []; 
+var countries = []; 
