f81d152fded304038335266fa0655ea9b7ec4464
Jason Fox <jason-fox@users.noreply.github.com>
2024-10-22 18:30:15 +0200

    Add dummy data

---
+++ directories/ihubs/mapData.js
@@ -0,0 +1,26 @@
+var iHub_data = {
+	"type":"FeatureCollection","name":"ihubs_4",
+	"crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},
+	"features":[
+	
+		{
+			"type":"Feature",
+			"properties":{
+				"html": [
+					"<tr><td colspan='6'><img class='featured-image' src='https://www.fiware.org/wp-content/directories/ihubs/images/nosc-ua-dih.png'></td></tr>",
+					"<tr><td class='ihub-location'>Kyiv<br>Ukraine</td><td><img class='flag' src='https://www.fiware.org/wp-content/directories/people/images/flag/flag_Ukraine.png'></td></tr>",
+					"<tr><td class='ihub-name' colspan='6'>NOSC-UA DIH FIWARE iHub</td></tr>",
+					"<tr><th colspan='6' scope='row' class='label'>Type</th></tr>",
+					"<tr><td colspan='6' class='visible-with-data attribute' id='Type'>Incubated</td></tr>",
+					"<tr><th colspan='6' scope='row' class='label'>Domain</th></tr>",
+					"<tr><td colspan='6' class='visible-with-data attribute' id='Domain'>Multidomain</td></tr>",
+					"<tr colspan='6'><td class='icons' colspan='1'>	<a target='_blank' href='http://cloud-5.bitp.kiev.ua/?page_id=1256&amp;lang=en'><span class='icon-website'></span></a><a target='_blank' href='https://www.linkedin.com/showcase/nosc-ua-dih/'><span class='icon-linkedin'></span></a></td></tr>"
+				]
+			},
+			"geometry":{
+				"type":"Point",
+				"coordinates":[-56.1851147,-34.9055016]
+			}
+		},
+	]
+};
