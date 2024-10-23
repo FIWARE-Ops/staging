0b1b50c9d3136391d0df727324dedd266fb306a4
Jason Fox <jason-fox@users.noreply.github.com>
2024-10-23 18:20:23 +0200

    Swap to MapLibre

--- directories/cities/app_cities_map.js
+++ directories/cities/app_cities_map.js
@@ -37,97 +37,86 @@ function horizontalScroll(popup) {
 
 function setBounds() {}
 
-var map = L.map("map", {
+const map = new maplibregl.Map({
+  container: 'map',
+  style: './style.json',
   zoomControl: false,
   maxZoom: 28,
   minZoom: 1,
 }).fitBounds([
-  [-56.093228369773406, -175.73934032129907],
-  [83.70561326982735, 179.54543205831558],
+  [-175.73934032129907, -56.093228369773406 ],
+  [179.54543205831558, 83.70561326982735 ],
 ]);
-var hash = new L.Hash(map);
-map.attributionControl.setPrefix(
-  '<a href="https://github.com/tomchadwin/qgis2web" target="_blank">qgis2web</a> &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot; <a href="https://qgis.org">QGIS</a>',
-);
-var zoomControl = L.control
-  .zoom({
-    position: "topleft",
-  })
-  .addTo(map);
-var bounds_group = new L.featureGroup([]);
 
+map.addControl(new maplibregl.NavigationControl());
 
-map.createPane("pane_mapcountries");
-map.getPane("pane_mapcountries").style.zIndex = 401;
-map.getPane("pane_mapcountries").style["mix-blend-mode"] = "normal";
-var layer_mapcountries = new L.geoJson(json_mapcountries, {
-  attribution: "",
-  interactive: false,
-  dataVar: "json_mapcountries",
-  layerName: "layer_mapcountries",
-  pane: "pane_mapcountries",
-  style: style_mapcountries,
+map.once("load", () => {
+  map.addSource("cities", {
+    type: "geojson",
+    data: "./cities.json",
+
+    cluster: true,
+    clusterRadius: 40 // cluster two trailheads if less than 20 pixels apart
+ 
+  });
+
+  map.addLayer({
+    id: "cities-circle",
+    type: "circle",
+    source: "cities",
+
+    paint: {
+      "circle-color": "hsla(5,80%,70%,0.5)",
+      "circle-stroke-width": 2,
+      "circle-radius": ["case", ["get", "cluster"], 10, 5],
+      "circle-stroke-color": "hsl(5,80%,50%)"
+    }
+  });
+
+  map.addLayer({
+    id: "cities-cluster-count",
+    type: "symbol",
+    source: "cities",
+    layout: {
+      "text-font": ["Arial Bold"],
+      "text-field": ["get", "point_count"],
+      "text-offset": [0, 0.1] // move the label vertically downwards slightly to improve centering
+    },
+    paint: {
+      "text-color": "white"
+    }
+  });
+
 });
-bounds_group.addLayer(layer_mapcountries);
-map.addLayer(layer_mapcountries);
 
-map.createPane("pane_mapboundaries");
-map.getPane("pane_mapboundaries").style.zIndex = 402;
-map.getPane("pane_mapboundaries").style["mix-blend-mode"] = "normal";
-var layer_mapboundaries = new L.geoJson(json_mapboundaries, {
-  attribution: "",
-  interactive: false,
-  dataVar: "json_mapboundaries",
-  layerName: "layer_mapboundaries",
-  pane: "pane_mapboundaries",
-  style: style_mapboundaries,
+map.on("mouseenter", "cities-circle", () => {
+  map.getCanvas().style.cursor = "pointer";
 });
-bounds_group.addLayer(layer_mapboundaries);
-map.addLayer(layer_mapboundaries);
 
-function pop_data(feature, layer) {
-  var content = feature.properties;
-  var tempDiv = document.createElement("div");
-  tempDiv.innerHTML = "<div>" + content.html.join("") + "</div>";
-  horizontalScroll(tempDiv);
-  layer.bindPopup(tempDiv, { minWidth: 400, maxHeight: 600 });
-}
-
-function style_data() {
-  return {
-    pane: "pane_data",
-    radius: 6.0,
-    stroke: false,
-    fill: true,
-    fillOpacity: 1,
-    fillColor: "rgba(93,192,207,1.0)",
-    interactive: true,
-  };
-}
-map.createPane("pane_data");
-map.getPane("pane_data").style.zIndex = 404;
-map.getPane("pane_data").style["mix-blend-mode"] = "normal";
-var layer_data = new L.geoJson(cities_data, {
-  attribution: "",
-  interactive: true,
-  dataVar: "cities_data",
-  layerName: "layer_data",
-  pane: "pane_data",
-  onEachFeature: pop_data,
-  pointToLayer: function (feature, latlng) {
-    var context = {
-      feature: feature,
-      variables: {},
-    };
-    return L.circleMarker(latlng, style_data(feature));
-  },
+map.on("mouseleave", "cities-circle", () => {
+  map.getCanvas().style.cursor = "";
 });
-var cluster_data = new L.MarkerClusterGroup({
-  showCoverageOnHover: false,
-  spiderfyDistanceMultiplier: 2,
-});
-cluster_data.addLayer(layer_data);
 
-bounds_group.addLayer(layer_data);
-cluster_data.addTo(map);
-setBounds();
+
+map.on("click", "cities-circle", (e) => {
+  const city = e.features[0]; 
+  if(city.properties.cluster){
+    const point = [e.lngLat.lng, e.lngLat.lat]
+    map.flyTo({
+      center: point
+    });
+    setTimeout(function(){
+       map.zoomIn()
+    }, 500);
+  } else {
+    const html = JSON.parse(city.properties.html);
+    const content = html.join('');
+
+    new maplibregl.Popup()
+      .setHTML(`<div> ${content}</div>`)
+      .setLngLat(city.geometry.coordinates)
+      .addTo(map);
+  }
+
+});
+
