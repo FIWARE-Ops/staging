778af1ba748b0d0311f627184bea1331692c4008
Jason Fox <jason-fox@users.noreply.github.com>
2024-10-30 11:33:41 +0100

    Update iHubs

--- directories/ihubs/app_iHubs_map.js
+++ directories/ihubs/app_iHubs_map.js
@@ -1,5 +1,7 @@
 function horizontalScroll(popup) {
   // Horizontal Scroll
+
+
   var sliders = popup.querySelectorAll(".chips");
   var isDown = false;
   var startX;
@@ -37,48 +39,77 @@ function horizontalScroll(popup) {
 
 function setBounds() {}
 
-const map = new maplibregl.Map({
-  container: 'map',
-  style: './style.json',
-  zoomControl: false,
-  maxZoom: 28,
-  minZoom: 1,
-}).fitBounds([
-  [-175.73934032129907, -56.093228369773406 ],
-  [179.54543205831558, 83.70561326982735 ],
-]);
+const isIhub = ['==', ['get', 'type'], 'ihub'];
+const isCity = ['==', ['get', 'type'], 'city'];
+const isMember = ['==', ['get', 'type'], 'Organization'];
 
-map.addControl(new maplibregl.NavigationControl());
-
-map.once("load", () => {
-  map.addSource("iHubs", {
+function addSource(name, source ) {
+  map.addSource(name, {
     type: "geojson",
-    data: "./iHubs.json",
+    data: source,
 
     cluster: true,
-    clusterRadius: 50
- 
+    clusterRadius: 50,
+    clusterMaxZoom: 5
   });
+}
 
+function addLayer(source){
+  if (map.getLayer('points')) map.removeLayer('points');
   map.addLayer({
-    id: "iHubs-circle",
+    id: "points",
     type: "circle",
-    source: "iHubs",
-
+    source,
+    'filter': ['!=', 'cluster', true],
     paint: {
-      "circle-color": ["case", ["boolean", ["get", "cluster"], false],"white", "cyan"],
-      "circle-stroke-width": ["case", ["boolean", ["get", "cluster"], false], 5, 0],
-      "circle-radius": ["case", ["boolean", ["get", "cluster"], false], 15, 5],
-      "circle-stroke-color": ["case", ["boolean", ["get", "cluster"], false], "silver", "cyan"]
+      "circle-color": ["case",
+         isCity, "rgba(255,255,255,0.3)",
+         isIhub, "rgba(93,192,207,.6)",
+         isMember, "rgba(0,27,61,1)", 
+         "rgba(93,192,207,.6)"],
+      "circle-stroke-width": ["case",
+        isCity, 1,
+        isIhub, 1.2,
+        isMember, 1, 
+        1.2],
+      "circle-stroke-color": ["case",
+         isCity, "rgba(255,255,255,1)",
+         isIhub, "rgba(93,192,207,1)",
+         isMember, "rgba(255,255,255,1)", 
+         "rgba(93,192,207,1)"],
+      "circle-radius": ["case", 
+        isCity, 7,
+        isIhub, 7,
+        isMember, 7,
+        7]
     }
   });
 
+  if (map.getLayer('clusters')){ 
+    map.removeLayer('clusters');
+  }
   map.addLayer({
-    id: "iHubs-cluster-count",
+    id: "clusters",
+    type: "circle",
+    source,
+    'filter': ['==', 'cluster', true],
+    paint: {
+      "circle-color": "white", 
+      "circle-stroke-width": 5,
+      "circle-radius": 15,
+      "circle-stroke-color":  "silver"
+    }
+  });
+
+  if (map.getLayer('cluster-count')) {
+    map.removeLayer('cluster-count');
+  }
+  map.addLayer({
+    id: "cluster-count",
     type: "symbol",
-    source: "iHubs",
+    source,
     layout: {
-      "text-font": ["Monserrat Bold, Arial Bold"],
+      "text-font": ["Montserrat Bold", "Arial Bold"],
       "text-size": 12,
       "text-field": ["get", "point_count"],
       "text-offset": [0, 0.1] // move the label vertically downwards slightly to improve centering
@@ -87,43 +118,152 @@ map.once("load", () => {
       "text-color": "black"
     }
   });
+}
 
-});
+let popups = null;
+let filter = "ihubs";
 
-map.on("mouseenter", "iHubs-circle", () => {
-  map.getCanvas().style.cursor = "pointer";
-});
+function initTextSearch() {
+  
 
-map.on("mouseleave", "iHubs-circle", () => {
-  map.getCanvas().style.cursor = "";
-});
+  document.querySelector(".resetInput").addEventListener("click", (el) => {
+    document.querySelector("#searchInput").value = "";
+    document.querySelector(".search-element").classList.remove("resetActive");
+  });
+
+  // Search input
+  document.querySelector("#searchInput").addEventListener("keyup", (e) => {
+    let searchKeys = searchObj.keys[filter]
+
+    console.log(searchKeys)
+
+    let location = null;
+    const reFuzzy = new RegExp(`.*${e.target.value}.*`, "gi");
+    const reExact = new RegExp(`^${e.target.value}`, "gi");
+
+    if (e.target.value != "") {
+      e.target.parentNode.classList.add("resetActive");
+    } else {
+      e.target.parentNode.classList.remove("resetActive");
+    }
+    if (e.target.value.length < 3) {
+      return;
+    }
+    
+    let matches = searchKeys.filter(value => reFuzzy.test(value));
+    if(matches.length === 1){
+      location = searchObj.data[matches[0]];
+    } else {
+      matches = searchKeys.filter(value => reExact.test(value));
+      if(matches.length === 1){
+        location = searchObj.data[matches[0]];
+      }
+    }
 
 
-map.on("click", "iHubs-circle", (e) => {
-  const iHub = e.features[0]; 
-  if(iHub.properties.cluster){
-    const point = [e.lngLat.lng, e.lngLat.lat]
-    map.flyTo({
-      center: point
-    });
-    setTimeout(function(){
-       map.zoomIn()
-    }, 500);
-  } else {
-    const html = JSON.parse(iHub.properties.html);
-    const content = html.join('');
 
-    new maplibregl.Popup()
-      .setHTML(`<div> ${content}</div>`)
-      .setLngLat(iHub.geometry.coordinates)
-      .addTo(map);
+    if(location){
+      
+      let bbox = [
+        [location[0]-1, location[1]-0.5], 
+        [location[0]+1, location[1]+1.5]];
 
-    setTimeout(function(){
-      horizontalScroll(document.querySelectorAll(".maplibregl-popup-content")[0])
-    }, 500);
+      map.fitBounds(bbox, {
+        padding: {top: 10, bottom:25, left: 15, right: 5}
+      });
 
+      
+ 
 
-  }
+      
+      map.once('idle', () => {
+        const features = map.querySourceFeatures(filter) 
 
-});
+        let targetFeature = {}
+        features.forEach((feature)=> {
+            if(feature.properties.city === matches[0]){
+              targetFeature = feature;
+            }
+            if(feature.properties.name === matches[0]){
+              targetFeature = feature;
+            }
+        });
 
+        const html = JSON.parse(targetFeature.properties.html);
+        const content = html.join('');
+        e.target.value = matches[0];
+
+        if(popups){popups.remove()}
+        popups = new maplibregl.Popup()
+          .setHTML(`<div class="popup-info"> ${content}</div>`)
+          .setLngLat(location);
+
+        popups.addTo(map);
+        horizontalScroll(document.querySelectorAll(".maplibregl-popup-content")[0])
+      });
+
+    }
+  });
+}
+
+function initMap() { 
+  
+
+  map.addControl(new maplibregl.NavigationControl());
+  map.addControl(new maplibregl.AttributionControl({compact: true}));
+  map.on("mouseenter", "points", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
+
+  map.on("mouseleave", "points", () => {
+    map.getCanvas().style.cursor = "";
+  });
+
+  map.on("click", "clusters", (e) => {
+      const point = [e.lngLat.lng, e.lngLat.lat];
+      map.flyTo({
+        center: point
+      });
+      setTimeout(function(){
+         map.zoomIn()
+      }, 500);
+  });
+
+  map.on("click", "points", (e) => {
+    const city = e.features[0]; 
+      const html = JSON.parse(city.properties.html);
+      const content = html.join('');
+
+      if(popups){popups.remove()}
+      popups = new maplibregl.Popup()
+        .setHTML(`<div class="popup-info"> ${content}</div>`)
+        .setLngLat(city.geometry.coordinates);
+
+      popups.addTo(map);
+
+      setTimeout(function(){
+        horizontalScroll(document.querySelectorAll(".maplibregl-popup-content")[0])
+      }, 500);
+    
+  });
+
+  map.once("load", () => {
+    // Add sources
+    addSource("ihubs","./ihubs.json");
+    addLayer("ihubs")
+  });
+}
+
+const map = new maplibregl.Map({
+    container: 'map',
+    style: './style.json',
+    zoomControl: false,
+    maxZoom: 28,
+    minZoom: 1,
+    attributionControl: false
+  }).fitBounds([
+    [-175.73934032129907, -56.093228369773406 ],
+    [179.54543205831558, 83.70561326982735 ],
+  ]);
+initMap();
+initTextSearch();
