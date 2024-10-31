7b7e7825b125438ccb937de3177da4305b7b2f58
Jason Fox <jason-fox@users.noreply.github.com>
2024-10-31 10:38:09 +0100

    Remove compass.

--- directories/organisations/app_organisations_map.js
+++ directories/organisations/app_organisations_map.js
@@ -257,10 +257,10 @@ function initMap() {
 const map = new maplibregl.Map({
     container: 'map',
     style: './style.json',
-    zoomControl: false,
-    maxZoom: 28,
-    minZoom: 1,
-    attributionControl: false
+    maxZoom: 20,
+    minZoom: 3,
+    attributionControl: false,
+    dragRotate: false
   }).fitBounds([
     [-175.73934032129907, -56.093228369773406 ],
     [179.54543205831558, 83.70561326982735 ],
