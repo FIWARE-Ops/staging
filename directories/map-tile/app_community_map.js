c26c20678ee9a50c9c7f55772a886700a5b0239f
Giacomo <reddy.depa@gmail.com>
2024-11-04 21:47:02 +0100

    map-tile upload

---
+++ directories/map-tile/app_community_map.js
@@ -0,0 +1,19 @@
+function setBounds() {}
+
+function initMap() { 
+  map.addControl(new maplibregl.NavigationControl());
+  map.addControl(new maplibregl.AttributionControl({compact: true}));
+}
+
+const map = new maplibregl.Map({
+    container: 'map',
+    style: './style.json',
+    maxZoom: 20,
+    minZoom: 1,
+    attributionControl: false,
+    dragRotate: false
+  }).fitBounds([
+    [-175.73934032129907, -56.093228369773406 ],
+    [179.54543205831558, 83.70561326982735 ],
+  ]);
+initMap();
