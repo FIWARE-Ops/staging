f2fb00bd9d83eeadf844b6c2baa0dea2b2dd7dd9
Jason Fox <jason-fox@users.noreply.github.com>
2024-11-09 07:50:26 +0100

    Add scroller

--- directories/events-directory/app_events.js
+++ directories/events-directory/app_events.js
@@ -184,6 +184,44 @@ function smoothScroll() {
 }
 
 
+function horizontalScroll() {
+  // Horizontal Scroll
+  var sliders = document.querySelectorAll(".speakers");
+  var isDown = false;
+  var startX;
+  var scrollLeft;
+  sliders.forEach(function (slider) {
+    slider.addEventListener("mousedown", function (e) {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener("mouseleave", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mouseup", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mousemove", function (e) {
+      if (!isDown) return;
+      e.preventDefault();
+      var x = e.pageX - slider.offsetLeft;
+      var walk = (x - startX) * 3; //scroll-fast
+
+      slider.scrollLeft = scrollLeft - walk;
+      var links = slider.querySelectorAll(".item");
+
+      for (var i = 0; i < links.length; i++) {
+        links[i].classList.add("noclick");
+      }
+    });
+  });
+}
+
+
 function initSticky() {
   window.onscroll = onScrollHandler;
 
@@ -214,6 +252,8 @@ document.addEventListener("html-included", () => {
   }
   init = true;
   initSelect();
+  horizontalScroll();
+  smoothScroll();
 
   //filterToggle();
   initSticky();
