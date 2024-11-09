d8d16362e340fc3f101a1157b0cd821d284435f2
GitHub Action <action@github.com>
2024-11-09 16:38:28 +0000

    Update Staging

--- directories/events-directory/pageData.js
+++ directories/events-directory/pageData.js
@@ -1,80 +1,73 @@
 window.eventData = [
     {
-        "id": "gitex-africa-2024",
+        "id": "202405-gitex-africa-2024",
         "title": "GITEX Africa 2024",
         "from": "2024-05-28T22:00:00.000Z",
         "to": "2024-05-30T22:00:00.000Z",
         "description": "FIWARE and Atos will be showcasing the joint value proposition that aligns digital innovation with environmental responsibility.",
         "location": "Place Bab Jdid Bd Al Yarmouk, Marrakesh, 40000 Morocco ",
         "isAllDay": true,
-        "group": "Conference",
-        "url": "./event-details?id=gitex-africa-2024"
+        "group": "Conference"
     },
     {
-        "id": "dach-netzwerktreffen-at-viennaup",
+        "id": "202406-dach-netzwerktreffen-at-viennaup",
         "title": "DACH Netzwerktreffen at ViennaUP",
         "from": "2024-06-05T08:00:00.000Z",
         "to": "2024-06-05T10:30:00.000Z",
         "description": "The FIWARE DACH Networking Event brings together German-speaking FIWARE enthusiasts for high-quality lectures and opportunities.",
         "location": "Impact Hub Lindengasse 56, 1070 Wien, Austria ",
         "isAllDay": false,
-        "group": "Networking",
-        "url": "./event-details?id=dach-netzwerktreffen-at-viennaup"
+        "group": "Networking"
     },
     {
-        "id": "advanced-ngsi-ld-registrations---using-ngsi-ld-in-a--system-of-systems--approach",
+        "id": "202406-advanced-ngsi-ld-registrations---using-ngsi-ld-in-a--system-of-systems--approach",
         "title": "Advanced NGSI-LD Registrations – Using NGSI-LD in a “System-of-Systems” Approach",
         "from": "2024-06-07T08:00:00.000Z",
         "to": "2024-06-07T09:00:00.000Z",
         "description": "A deep dive into using registrations effectively, and best practice for data spaces and system of systems with NGSI-LD.",
         "location": "Online",
         "isAllDay": false,
-        "group": "FIWARE Webinars",
-        "url": "./event-details?id=advanced-ngsi-ld-registrations---using-ngsi-ld-in-a--system-of-systems--approach"
+        "group": "FIWARE Webinars"
     },
     {
-        "id": "cities-climate-action-summit-2024",
+        "id": "202406-cities-climate-action-summit-2024",
         "title": "Cities Climate Action Summit 2024",
         "from": "2024-06-26T07:00:00.000Z",
         "to": "2024-06-27T16:00:00.000Z",
         "description": "The CCAS24 is the leading event focused on climate action in cities, FIWARE is proud to partner with Smart Cities World.",
         "location": "County Hall Belvedere Rd, London SE1 7GP, Regno Unito ",
         "isAllDay": false,
-        "group": "Summit",
-        "url": "./event-details?id=cities-climate-action-summit-2024"
+        "group": "Summit"
     },
     {
-        "id": "smart-city-expo-world-congress-2024",
+        "id": "202411-smart-city-expo-world-congress-2024",
         "title": "Smart City Expo World Congress 2024",
         "from": "2024-11-04T23:00:00.000Z",
         "to": "2024-11-06T23:00:00.000Z",
         "description": "As an official Event Partner, we are honoured to be present in Pavilion 3, Stand B11, with the FIWARE Community booth.",
         "location": "Fira Barcelona Gran Via Av. Joan Carles I, 64, 08908 L&#x27;Hospitalet de Llobregat, Barcelona, Spain ",
         "isAllDay": true,
-        "group": "Conference",
-        "url": "./event-details?id=smart-city-expo-world-congress-2024"
+        "group": "Conference"
     },
     {
-        "id": "fiware-data-space-technologies-webinar--6--ongoing-and-future-work",
+        "id": "202411-fiware-data-space-technologies-webinar--6--ongoing-and-future-work",
         "title": "FIWARE Data Space Technologies Webinar #6: Ongoing and Future Work",
         "from": "2024-11-21T14:30:00.000Z",
         "to": "2024-11-21T16:00:00.000Z",
         "description": "Get a sneak peek into the support of data space protocols, the DOME project and, upcoming innovations and ongoing work.",
         "location": "Online",
         "isAllDay": false,
-        "group": "FIWARE Webinars",
-        "url": "./event-details?id=fiware-data-space-technologies-webinar--6--ongoing-and-future-work"
+        "group": "FIWARE Webinars"
     },
     {
-        "id": "data-spaces-symposium-2024",
+        "id": "202503-data-spaces-symposium-2024",
         "title": "Data Spaces Symposium 2024",
         "from": "2025-03-11T23:00:00.000Z",
         "to": "2025-03-13T23:00:00.000Z",
         "description": "This year’s edition will show market-ready use cases and advanced technology solutions for existing and future data spaces.",
         "location": "Darmstadtium - Science and Congress Center Schlossgraben 1, 64283 Darmstadt, Germany ",
         "isAllDay": true,
-        "group": "Symposium",
-        "url": "./event-details?id=data-spaces-symposium-2024"
+        "group": "Symposium"
     }
 ];
 
