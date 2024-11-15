9af6120d1ff0dfeae0dc9db8009693ff65219443
GitHub Action <action@github.com>
2024-11-15 08:49:53 +0000

    Update Staging

--- directories/events-directory/pageData.js
+++ directories/events-directory/pageData.js
@@ -27,7 +27,29 @@ window.eventData = [
         "description": "A deep dive into using registrations effectively, and best practice for data spaces and system of systems with NGSI-LD.",
         "location": "Online",
         "isAllDay": false,
-        "group": "FIWARE Webinars"
+        "group": "Webinar"
+    },
+    {
+        "id": "202406-fiware-data-spaces---a-practical-introduction-into-roles-and-components",
+        "title": "FIWARE Data Spaces – A Practical Introduction Into Roles and Components",
+        "from": "2024-06-14T08:00:00.000Z",
+        "to": "2024-06-14T09:00:00.000Z",
+        "description": "This webinar will introduce the fundamentals of a Data Space built with the FIWARE Data Space Connector. After the webinar, you will be able to install a Minimal Data Space locally and understand the essential building blocks used for it.",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
+        "id": "202406-leveraging-ngsi-in-university-courses--",
+        "title": "Leveraging NGSI in University Courses
+
+",
+        "from": "2024-06-21T08:00:00.000Z",
+        "to": "2024-06-21T09:00:00.000Z",
+        "description": "This FIWARE Webinar explores the available FIWARE materials and demonstrates how to combine free-to-use generic enablers with other existing data protocols to craft an inspiring curriculum for teaching software skills to students.",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
     },
     {
         "id": "202406-cities-climate-action-summit-2024",
@@ -40,6 +62,85 @@ window.eventData = [
         "group": "Summit"
     },
     {
+        "id": "202407-creating-combined-ngsi-v2-ngsi-ld-data-spaces---",
+        "title": "Creating Combined NGSI-v2/NGSI-LD Data Spaces 
+
+",
+        "from": "2024-07-05T08:00:00.000Z",
+        "to": "2024-07-05T09:00:00.000Z",
+        "description": "Discover how context brokers can effectively combine the NGSI-v2 and NGSI-LD formats to take advantage of linked-data and non-linked-data systems within their domain.",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
+        "id": "202407-advanced-dashboards-with-wirecloud-2-0--",
+        "title": "Advanced Dashboards with WireCloud 2.0
+
+",
+        "from": "2024-07-19T08:00:00.000Z",
+        "to": "2024-07-19T09:00:00.000Z",
+        "description": "This FIWARE Webinar will guide you through the essentials of setting up Wirecloud, crafting customized dashboards, and integrating data from FIWARE.",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
+        "id": "202409-enhancing-tourism-with-smart-technologies--integrating-fiware-and-large-language-models--",
+        "title": "Enhancing Tourism with Smart Technologies: Integrating FIWARE and Large Language Models 
+",
+        "from": "2024-09-06T08:00:00.000Z",
+        "to": "2024-09-06T09:00:00.000Z",
+        "description": "This FIWARE Webinar will delve into the integration of FIWARE with the powerful capabilities of Large Language Models (LLMs) equipped with Retrieval Augmented Generation (RAG).",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
+        "id": "202409-fiware-tech-training-naples--",
+        "title": "FIWARE Tech Training Naples
+
+",
+        "from": "2024-09-17T22:00:00.000Z",
+        "to": "2024-09-18T22:00:00.000Z",
+        "description": "Get equipped with the necessary skills to develop Smart Solutions based on FIWARE!",
+        "location": "Mostra d&#x27;Oltremare 54 Viale John Fitzgerald Kennedy ",
+        "isAllDay": true,
+        "group": "Summit"
+    },
+    {
+        "id": "202409-fiware-global-summit-2024--",
+        "title": "FIWARE Global Summit 2024
+
+",
+        "from": "2024-09-17T22:00:00.000Z",
+        "to": "2024-09-18T22:00:00.000Z",
+        "description": "Mark your calendars: the 10th Anniversary FIWARE Global Summit will take place on September 18-19, 2024 in Naples, Italy! Join pioneers from open source companies, cities, universities, and developers shaping the digital economy. This is a must-attend event for entrepreneurs, public administrations, academia, startups, and technologists.",
+        "location": "Mostra d&#x27;Oltremare 54 Viale John Fitzgerald Kennedy ",
+        "isAllDay": true,
+        "group": "Summit"
+    },
+    {
+        "id": "202409-fi-dinner-naples",
+        "title": "FI-Dinner Naples",
+        "from": "2024-09-18T18:00:00.000Z",
+        "to": "2024-09-18T21:00:00.000Z",
+        "description": "Round out the FIWARE Global Summit in Style at the FI-Dinner.",
+        "location": "Palazzo Petrucci Ristorante 16 C Via Posillipo 80123 ",
+        "isAllDay": false,
+        "group": "Networking"
+    },
+    {
+        "id": "202410-fiware-data-space-technologies-webinar--1--fiware-vision-on-data-spaces",
+        "title": "FIWARE Data Space Technologies Webinar #1: FIWARE Vision on Data Spaces",
+        "from": "2024-10-31T14:30:00.000Z",
+        "to": "2024-10-31T16:00:00.000Z",
+        "description": "An introduction to the value proposition of FIWARE for data spaces and what standards are needed and have been implemented in FIWARE for enabling secure, interoperable exchange based on data services offered for the creation of value.",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
         "id": "202411-smart-city-expo-world-congress-2024",
         "title": "Smart City Expo World Congress 2024",
         "from": "2024-11-04T23:00:00.000Z",
@@ -50,6 +151,16 @@ window.eventData = [
         "group": "Conference"
     },
     {
+        "id": "202411-transforming---exploiting-high-value-datasets--synergies-in-eu-open-data-projects",
+        "title": "Transforming &amp; Exploiting High Value Datasets: Synergies in EU Open Data Projects",
+        "from": "2024-11-19T09:00:00.000Z",
+        "to": "2024-11-19T11:00:00.000Z",
+        "description": "This event showcases the collaborative efforts of four pioneering EU-funded projects; Open Maps for Europe 2 (OME2), MAREGRAPH, RODEO, and BeOpen, to revolutionise the landscape of High Value Datasets (HVDs).",
+        "location": "Online",
+        "isAllDay": false,
+        "group": "Webinar"
+    },
+    {
         "id": "202411-fiware-data-space-technologies-webinar--5--reference-use-case",
         "title": "FIWARE Data Space Technologies Webinar #5: Reference Use Case",
         "from": "2024-11-19T14:30:00.000Z",
@@ -57,7 +168,7 @@ window.eventData = [
         "description": "This session will dive into a reference use case detailing the description of the participants and conclude with a deployment aligning to the business needs of the use case.",
         "location": "Online",
         "isAllDay": false,
-        "group": "FIWARE Webinars"
+        "group": "Webinar"
     },
     {
         "id": "202411-fiware-data-space-technologies-webinar--6--ongoing-and-future-work",
@@ -67,7 +178,7 @@ window.eventData = [
         "description": "Get a sneak peek into the support of data space protocols, the DOME project and, upcoming innovations and ongoing work.",
         "location": "Online",
         "isAllDay": false,
-        "group": "FIWARE Webinars"
+        "group": "Webinar"
     },
     {
         "id": "202503-data-spaces-symposium-2024",
