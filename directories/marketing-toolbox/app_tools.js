32a12ba9efeee30eccb245140f92b217e762c70f
Jason Fox <jason-fox@users.noreply.github.com>
2024-12-09 16:51:09 +0100

    Add JS

--- directories/marketing-toolbox/app_tools.js
+++ directories/marketing-toolbox/app_tools.js
@@ -391,10 +391,33 @@ function initSticky() {
   }
 }
 
+function setDropdown() {
+  $.urlParam = function (name) {
+    var results = new RegExp("[?&]" + name + "=([^&#]*)").exec(
+      window.location.href,
+    );
+    if (results == null) {
+      return null;
+    }
+    return decodeURI(results[1]) || 0;
+  };
+
+
+  if ($.urlParam("language")){
+    $("#filterCountry").val($.urlParam("language"));
+    return $("#filterCountry").change();
+  } else if ($.urlParam("type")){
+    $("#filterRole").val($.urlParam("type"));
+    return $("#filterRole").change();
+  } else if ($.urlParam("domain")){
+    $("#filterDomain").val($.urlParam("domain"));
+    return $("#filterDomain").change();
+  } else {
+    msnry.arrange({ sortBy: "original-order" });
+  }
+}
+
 document.addEventListener("html-included", () => {
-  //$("#filteredCompanies").text(window.modalData.length);
-  horizontalScroll();
-  smoothScroll();
   $("#app").css("visibility", "visible");
   if (init) {
     return;
@@ -404,31 +427,16 @@ document.addEventListener("html-included", () => {
   initChips();
   filterToggle();
   initSticky();
-  let count = 0;
-  let target = 7;
-  // Isotope istantiation
-  // Relies on unpkg.com/imagesloaded
-  $("#app")
-    .imagesLoaded()
-    .always(function (instance) {
-      msnry.arrange({ sortBy: "original-order" });
-      msnry.on("arrangeComplete", (filteredItems) => {
-        $("#filteredCompanies").text(filteredItems.length);
-        dropdownFilters(selectors);
-        highlightChips();
-        if (scrollSet) {
-          scrollToView();
-        }
-      });
-    })
-    .fail( function() {
-      // msnry.arrange({ sortBy: "original-order" });
-    })
-    .progress(function (instance, image) {
-      count++;
-      if(count % target === 0){
-        target = target + 7;
-        msnry.arrange({ sortBy: "original-order" });
-      }
-    });
-});
+  horizontalScroll();
+  smoothScroll();
+  msnry.on("arrangeComplete", (filteredItems) => {
+    $("#filteredCompanies").text(filteredItems.length);
+    dropdownFilters(selectors);
+    highlightChips();
+    if (scrollSet) {
+      scrollToView();
+    }
+  });
+  setDropdown();
+ 
+}, {once: true});
