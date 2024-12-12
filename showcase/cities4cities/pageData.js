76380cb58a99ba69e655ad81adbf093e9b487c9b
GitHub Action <action@github.com>
2024-12-12 11:15:55 +0000

    Update Staging

--- showcase/cities4cities/pageData.js
+++ showcase/cities4cities/pageData.js
@@ -9,7 +9,7 @@ function shuffle(sourceArray) {
   return sourceArray;
 }
 
-var members = [{"company":"City of Vienna","name":"Smart Data Wien","img":"https://www.fiware.org/style/imgs/Marketplace/product/cities4cities/logo_CityOfVienna.svg","fiwareMember":true,"fiwareIhub":false,"companyLink":"../product-details/?category=cities&id=cityofvienna-smartdatawien","domain":["Cities","Government"],"type":"","technology":["Internet of Things","Mobility and Location","Open Data"],"year":2017,"content":"Urban Data Platform of the City of Vienna"}];
+var members = [{"company":"City of Vienna","name":"Smart Data Wien","logo":"logo_CityOfVienna.svg","fiwareMember":true,"fiwareIhub":false,"companyLink":"../product-details/?category=cities&id=cityofvienna-smartdatawien","domain":["Cities","Government"],"type":"","technology":["Internet of Things","Mobility and Location","Open Data"],"year":2017,"content":"Urban Data Platform of the City of Vienna","logoUrl":"https://www.fiware.org/wp-content/directories/showcase/cities4cities/logo/logo_CityOfVienna.svg"}];
 var nonMembers = [];
 var pageData = shuffle(members).concat(shuffle(nonMembers));
 
