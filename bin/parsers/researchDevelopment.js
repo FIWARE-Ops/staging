3ffb3f44fd47b73f74a58918c91fcb1ebd04f45b
Giacomo <reddy.depa@gmail.com>
2024-12-12 17:04:26 +0100

    researchDevelopment eu-table rename

--- bin/parsers/researchDevelopment.js
+++ bin/parsers/researchDevelopment.js
@@ -198,7 +198,7 @@ function generateHTML(projects) {
 async function generateInternalHTML(projects) {
     await Template.write(
         path.join(INTERNAL_R_AND_D_DIR, 'eu-projects.html'),
-        path.join(TEMPLATE_PATH, 'table.hbs'),
+        path.join(TEMPLATE_PATH, 'eu-table.hbs'),
         _.map(projects, _.clone)
     );
     Prettier.format(path.join(INTERNAL_R_AND_D_DIR, 'eu-projects.html'), { parser: 'html' });
