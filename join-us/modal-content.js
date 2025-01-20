03f570929835cebbbd32b676b40d82ce94e63975
Giacomo <reddy.depa@gmail.com>
2025-01-20 17:26:33 +0100

    join us folder updates

---
+++ join-us/modal-content.js
@@ -0,0 +1,53 @@
+// here's goes the modal content
+
+var modalData = [
+  {
+    "name" : "FIWARE for Impact",
+    "img": "https://www.fiware.org/style/imgs/join-us/icons/icon-fiware-for-impact.svg",
+    "badge": "https://www.fiware.org/style/imgs/join-us/badges/badge-fiware-for-impact.svg",
+    "content": "<p>At FIWARE Foundation, we are dedicated to supporting you throughout your journey to success. From the very beginning to the achievements you earn, we will be with you every step of the way. Join the FIWARE Community and unlock endless possibilities through our extensive reach, broad network, exclusive marketplace, webinars and workshops, diverse promotional offerings, and participation in renowned international events.</p><h2>Program Services</h2><ul class='program-services'><li>Welcome Kit</li><li>Introduction in the Community Round-up call</li><li>Joint new member blog posts</li><li>Promotion in FIWARE Newsletter</li><li>Promotion on FIWARE SoMe Channels</li><li>Guest speakers slot(s)</li><li>Free events ticket(s)</li><li>Discounted booth spot</li><li>FIWARE Impact Stories</li><li>Press Releases</li><li>Dedicated page on fiware.org</li><li>Involvement on the FIWARE publications</li></ul>"
+  },
+{
+    "name" : "FIWARE for Developers",
+    "img": "https://www.fiware.org/style/imgs/join-us/icons/icon-fiware-for-developers.svg",
+    "badge": "https://www.fiware.org/style/imgs/join-us/badges/badge-fiware-for-developers.svg",
+    "content": "Stay ahead of the game by leveraging our carefully curated, open-source framework of software platform components to develop innovative, interoperable, and replicable smart solutions. Enhance your understanding of each component's usage through our ongoing series of exclusive FIWARE Webinars and bespoke training and coaching opportunities. Additionally, enjoy priority access to technical support and quality assurance services. Once you have acquired enough know-how about FIWARE, level up to become acknowledged as a certified FIWARE Expert and unlock access to additional funding opportunities.</p><h2>Program Services</h2><ul class='program-services'><li>Involvement in Tech Webinars</li><li>Tech Webinar’s full recording available</li><li>Quality Assurance services</li><li>Dedicated Q&A line</li><li>Open Calls (funding) for FIWARE Experts</li><li>Trainings (classroom)</li><li>Smart Data Models support (priority)</li><li>TSC meetings</li><li>Focus Groups on Sectors</li></ul>"
+  },
+{
+    "name" : "FIWARE for Academia",
+    "img": "https://www.fiware.org/style/imgs/join-us/icons/icon-fiware-for-academia.svg",
+    "badge": "https://www.fiware.org/style/imgs/join-us/badges/badge-fiware-for-academia.svg",
+    "content": "Join the FIWARE Scientific Advisory Board (FISAB) and play a pivotal role in shaping the strategic direction of FIWARE Foundation. Unleash the potential of your university's future developers with the FIWARE University Roadshow. Our services aim to empower students by expanding their skills and knowledge of open-source technologies and open data. Other opportunities include exciting hackathons aimed at developing new and innovative smart services and solutions.</p><h2>Program Services</h2><ul class='program-services'><li>MSCs participation</li><li>FIWARE Advocates</li><li>Special open calls</li><li>EU funding provision</li><li>FIWARE Showcase</li></ul>"
+  },
+{
+    "name" : "FIWARE for Growth",
+    "img": "https://www.fiware.org/style/imgs/join-us/icons/icon-fiware-for-growth.svg",
+    "badge": "https://www.fiware.org/style/imgs/join-us/badges/badge-fiware-for-growth.svg",
+    "content": "Supercharge your growth journey by unlocking the most promising open calls that you can respond to using FIWARE technology. Stay updated on the latest internationally funded projects and enhance your chances of successful participation. Join our exclusive informational sessions to learn more about European Commission topics related to FIWARE technology. Connect with other members who share your interests and express your willingness to contribute to exciting proposals.</p><h2>Program Services</h2><ul class='program-services'><li>FISAB</li><li>University Roadshow</li></ul>",
+  },
+{  
+  	"name" : "Platinum",
+    "company": "Details",
+    "company-link": "https://www.fiware.org/foundation/bylaws/",
+  	"content": "At FIWARE Foundation, we are dedicated to supporting you throughout your journey to success. From the very beginning to the achievements you earn, we will be with you every step of the way. Join the FIWARE Community and unlock endless possibilities through our extensive reach, broad network, exclusive marketplace, webinars and workshops, diverse promotional offerings, and participation in renowned international events."
+  },
+{
+    "name" : "Gold",
+    "company": "Details",
+    "company-link": "https://www.fiware.org/foundation/bylaws/",
+    "content": "Stay ahead of the game by leveraging our carefully curated, open-source framework of software platform components to develop innovative, interoperable, and replicable smart solutions. Enhance your understanding of each component's usage through our ongoing series of exclusive FIWARE Webinars and bespoke training and coaching opportunities. Additionally, enjoy priority access to technical support and quality assurance services. Once you have acquired enough know-how about FIWARE, level up to become acknowledged as a certified FIWARE Expert and unlock access to additional funding opportunities."
+  },
+{
+    "name" : "Associate",
+    "company": "Details",
+    "company-link": "https://www.fiware.org/foundation/bylaws/",
+    "content": "Join the FIWARE Scientific Advisory Board (FISAB) and play a pivotal role in shaping the strategic direction of FIWARE Foundation. Unleash the potential of your university's future developers with the FIWARE University Roadshow. Our services aim to empower students by expanding their skills and knowledge of open-source technologies and open data. Other opportunities include exciting hackathons aimed at developing new and innovative smart services and solutions."
+  },
+{
+    "name" : "Individual",
+    "company": "Details",
+    "company-link": "https://www.fiware.org/foundation/bylaws/",
+    "content": "Join the FIWARE Scientific Advisory Board (FISAB) and play a pivotal role in shaping the strategic direction of FIWARE Foundation. Unleash the potential of your university's future developers with the FIWARE University Roadshow. Our services aim to empower students by expanding their skills and knowledge of open-source technologies and open data. Other opportunities include exciting hackathons aimed at developing new and innovative smart services and solutions."
+  }
+
+]
