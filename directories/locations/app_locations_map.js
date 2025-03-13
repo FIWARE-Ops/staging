009dc1b1d5c6e208a870ecb3b93893db13bcb162
Jason Fox <jason-fox@users.noreply.github.com>
2025-03-13 17:07:17 +0100

    Add locations

---
+++ directories/locations/app_locations_map.js
@@ -0,0 +1,135 @@
+function horizontalScroll(popup) {
+  // Horizontal Scroll
+
+  const sliders = popup.querySelectorAll(".chips");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  sliders.forEach(function (slider) {
+    slider.addEventListener("mousedown", function (e) {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener("mouseleave", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mouseup", function () {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mousemove", function (e) {
+      if (!isDown) {
+        return;
+      }
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+
+      slider.scrollLeft = scrollLeft - walk;
+      const links = slider.querySelectorAll(".item");
+
+      for (let i = 0; i < links.length; i++) {
+        links[i].classList.add("noclick");
+      }
+    });
+  });
+}
+
+function addSource(name, source) {
+  map.addSource(name, {
+    type: "geojson",
+    data: source,
+    cluster: false,
+  });
+}
+
+function addLayer(source) {
+  if (map.getLayer("points")) {
+    map.removeLayer("points");
+  }
+  map.addLayer({
+    id: "points",
+    type: "circle",
+    source,
+    paint: {
+      "circle-color": "rgba(93,192,207,.6)",
+      "circle-stroke-width": 1,
+      "circle-stroke-color":  "rgba(93,192,207,1)",
+      "circle-radius": 7,
+    },
+  });
+}
+
+let popups = null;
+const filter = "cities";
+
+
+function initMap() {
+  map.addControl(new maplibregl.NavigationControl());
+  map.addControl(new maplibregl.AttributionControl({ compact: true }));
+  map.on("mouseenter", "points", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
+
+  map.on("mouseleave", "points", () => {
+    map.getCanvas().style.cursor = "";
+  });
+
+  map.on("click", "clusters", (e) => {
+    const point = [e.lngLat.lng, e.lngLat.lat];
+    map.flyTo({
+      center: point,
+    });
+    setTimeout(function () {
+      map.zoomIn();
+    }, 500);
+  });
+
+  map.on("click", "points", (e) => {
+    const city = e.features[0];
+    const html = JSON.parse(city.properties.html);
+    const content = html.join("");
+
+    if (popups) {
+      popups.remove();
+    }
+    popups = new maplibregl.Popup()
+      .setHTML(`<div class="popup-info"> ${content}</div>`)
+      .setLngLat(city.geometry.coordinates);
+
+    popups.addTo(map);
+
+    setTimeout(function () {
+      horizontalScroll(
+        document.querySelectorAll(".maplibregl-popup-content")[0],
+      );
+    }, 500);
+  });
+
+  map.on("load", function () {
+    map.resize();
+  });
+
+  map.once("load", () => {
+    // Add sources
+    addSource("locations", "./locations.json");
+    addLayer("locations");
+  });
+}
+
+const map = new maplibregl.Map({
+  container: "map",
+  style: "./style.json",
+  maxZoom: 20,
+  minZoom: 3,
+  attributionControl: false,
+  dragRotate: false,
+}).fitBounds([
+  [-175.73934032129907, -56.093228369773406],
+  [179.54543205831558, 83.70561326982735],
+]);
+initMap();
+
