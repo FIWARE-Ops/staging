e81bbdcebf91688588bf2395546ec1ba689da6bd
Jason Fox <jason-fox@users.noreply.github.com>
2025-05-20 16:37:21 +0100

    Fixing Shuttle on STAGING

--- directories/fiware-summit/locations/app_locations_map.js
+++ directories/fiware-summit/locations/app_locations_map.js
@@ -25,7 +25,7 @@ function addLayer(source) {
   type: "symbol",
   source,
   layout: {
-    "icon-image": ["case", isVenue, 'venue', isPOI, 'attraction',  isHotel, 'hotel',  isRestaurant, 'restaurant', isAirport, 'airport',  isShuttle, 'shuttle', 'trainStation'],
+    "icon-image": ["case", isVenue, 'venue', isPOI, 'attraction',  isHotel, 'hotel',  isRestaurant, 'restaurant', isAirport, 'airport',  isShuttleBus, 'shuttle', 'trainStation'],
     "icon-size": ["case", isVenue, 0.3, 0.15],
     "icon-overlap": "always"
   },
