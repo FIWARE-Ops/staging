ee6dc0cc8c9fa1bddd89268a2495dd8196f4245b
Giacomo <reddy.depa@gmail.com>
2025-06-24 15:41:18 +0200

    matteo piselli and cristiana mapelli upload

--- directories/fiware-summit/speakers/pageData.js
+++ directories/fiware-summit/speakers/pageData.js
@@ -1,13 +1,16 @@
 var companies = [
-        'FIWARE Foundation',
+        'WiseTown',
 ]; var departments = [
 ]; var domains = [
 ]; var titles = [
-        'Architect',
-        'Evangelist',
+        'Journalist',
+        'Manager',
 ]; var countries = [
 ]; var modalData = [
-    { name: "Ken Zangelin", img: "https://www.fiware.org/wp-content/directories/people/images/200px/ken-zangelin.jpg", position: "Senior Architect &amp; Evangelist", company: "FIWARE Foundation", "company-link": "https://www.fiware.org/",
-    content: "Ken Zangelin (m) holds a Master of Science degree from the Royal Institute of Technology (KTH) in Stockholm, Sweden. He has +30 years of experience in programming using C, C++, shell in the Unix environment. During these +30 years, Ken has done a little bit of everything, including Real-Time Operating System design and implementation, Hardware-close programming, Boot-blocks, Board Support Packages, Interrupt routines and handling, TCP/IP Client-Server implementations, device drivers, compilers, interpreters, REST servers, ... you name it. Ken has been developing the Orion and Orion-LD Context Brokers, first at Telefonica R&amp;D (Orion) and after that, from 2017 and onwards, the Orion-LD context broker - “linked data context broker” for the FIWARE Foundation, Berlin, Germany. He’s also an active member of ETSI ISG CIM where the NGSI-LD standard is defined.", linkedin: "https://www.linkedin.com/in/kzangeli/", twitter: "", domain: "", location: ""
+    { name: "Cristiana Mapelli", img: "https://www.fiware.org/wp-content/directories/people/images/200px/cristiana-mapelli.jpg", position: "Journalist, Press Office", company: "", "company-link": "",
+    content: "", linkedin: "https://it.linkedin.com/in/cristianamapelli", twitter: "", domain: "", location: ""
+    },
+    { name: "Matteo Piselli", img: "https://www.fiware.org/wp-content/directories/people/images/200px/matteo-piselli.jpg", position: "Marketing &amp; Comms Manager", company: "WiseTown", "company-link": "https://wise.town/en/",
+    content: "", linkedin: "https://it.linkedin.com/in/matteopiselli", twitter: "", domain: "", location: ""
     }
 ];
