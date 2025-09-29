8463f44d16f3de14e7e2c0e46c752cbb022f7168
GitHub Action <action@github.com>
2025-09-29 11:40:31 +0000

    Update Staging

--- directories/people/bod/pageData.js
+++ directories/people/bod/pageData.js
@@ -1,50 +1,47 @@
 var companies = [
-        'ADDIX',
-        'Ficodes',
+        'Artech International',
         'Libelium',
         'Madinah Region Development Authority',
         'MedITech',
         'NEC',
-        'Secmotic',
         'Telefonica',
+        'Ubiwhere',
 ]; var departments = [
 ]; var domains = [
         'Gold',
         'Platinum',
 ]; var titles = [
         'CEO',
-        'Co-Founder',
         'CSO',
-        'CTO',
         'Director',
         'Fellow',
         'General Manager',
-        'Managing Director',
 ]; var countries = [
-        'Germany',
+        'Belgium',
         'Italy',
         'Japan',
+        'Portugal',
         'Saudi Arabia',
         'Spain',
 ]; var modalData = [
-    { name: "José Benitez", img: "https://www.fiware.org/wp-content/directories/people/images/200px/jose-benitez.jpg", position: "Co-Founder &amp; Chief Executive Officer", company: "Secmotic", "company-link": "https://secmotic.com/",
-    content: "Jose has been Secmotic’s CEO since 2014 and founder of FIWOO, a smart city platform based on FIWARE, created in 2021. He has been a member of the FIWARE Board of Directors since 2018, being keen at any moment in time to represent the vision of SMEs and their interests. Jose is a vibrant and active player in the FIWARE Community and is deeply motivated bringing in cutting edge innovation technologies. As a Telecommunications Engineer, he is continuously investigating what’s happening in the space of data acquisition through wireless communications such as 5G, LoRA, WiFi, LTE, among others.", linkedin: "https://www.linkedin.com/in/jose-benitez-secmotic", twitter: "https://twitter.com/josesecmotic", domain: "Gold", location: "Spain"
-    ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Spain.png"
+    { name: "Suna Akbayir", img: "https://www.fiware.org/wp-content/directories/people/images/200px/suna-akbayir.jpg", position: "Chief Executive Officer", company: "Artech International BVBA", "company-link": "https://artechconsulting.net/",
+    content: "Suna Akbayir is a Turkish entrepreneur with an extensive international business presence, well-recognized in Europe’s R&amp;D and innovation ecosystem. She is the Founder &amp; CEO of Artech International BVBA, a Brussels- headquartered in Brussels consulting and technology firm with an R&amp;D office in Leuven specializing in AI-driven solutions, Digital Product Passports (DPPs), and regulatory technology (RegTech) for industries such as automotive, electronics, and healthcare. With deep expertise in R&amp;D funding, technology transfer, and business development, she has led and contributed to multi-million-euro EU-funded projects, working closely with global industry leaders, SMEs, and policymakers. Through Artech, she has been at the forefront of innovation commercialization and market expansion, positioning the company as a key partner in Horizon Europe, Eureka, ITEA, and Celtic+ initiatives. Her expertise spans advisory roles, project evaluations, and mentoring for startups, SMEs, and research institutions, with a focus on bridging technology, business, and policy to drive real-world impact. She is known for her pragmatic approach and strategic foresight, actively shaping Europe’s innovation landscape. She has also been acting as the principal R&amp;D consultant for International Programs for Vestel Electronics, Vestel Home Appliances, Koç Sistem, Koç Digital, and Sestek; University of Maastrciht, RWTH Aachen etc.  She holds an MBA and a bachelor’s degree in Political Science and International Relations and speaks English, French, and Turkish.", linkedin: "https://www.linkedin.com/in/sunaakbayir/", twitter: "", domain: "Gold", location: "Belgium"
+    ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Belgium.png"
     },
     { name: "Agustín Cardenas Fernandez", img: "https://www.fiware.org/wp-content/directories/people/images/200px/agustin-cardenas-fernandez.jpg", position: "Director of Company Business Transformation", company: "Telefonica S.A.", "company-link": "https://www.telefonica.com/en/",
     content: "", linkedin: "https://www.linkedin.com/in/agucardenas/", twitter: "https://twitter.com/agucardenas", domain: "Platinum", location: "Spain"
     ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Spain.png"
     },
-    { name: "Francisco de la Vega", img: "https://www.fiware.org/wp-content/directories/people/images/200px/francisco-de-la-vega.jpg", position: "Chief Technology Officer", company: "Ficodes", "company-link": "https://www.ficodes.com/en/",
-    content: "Francisco de la Vega (male) is co-founder and CTO of the spanish startup FICODES as well as FIWARE architect. Francisco held a Master of Science in Computer Science from Universidad Politécnica de Madrid in 2015. Currently, he is making his Ph.D. in Computer Science in Universidad Politécnica de Madrid focusing on digital service management and monetization, automated revenue sharing, and value chain aggregation. As a researcher of the Computer Networks and Web Technologies Laboratory (CoNWeT Lab.) of UPM, he was engaged in several EU projects around the FIWARE platform starting in 2012, including FI-WARE and FI-CORE. Later, as CTO of FICODES he has been involved in multiple projects as FIWARE Architect, with a special focus in logistics, Smart Port and Smart City platforms. He has been actively participating on the FIWARE Community initially as the FIWARE Business Framework GEs owner (Store, Marketplace, Repository, and Revenue Settlement and Sharing system), and later as the FIWARE/TMF Business API Ecosystem GE owner, which integrates the aforementioned GEs with standard TMForum technologies. He is a member of the Data/API Management, Publication and Monetization FIWARE’s Technical Steering Committee.", linkedin: "https://www.linkedin.com/in/fdelavegagarcia", twitter: "https://twitter.com/frandlvega", domain: "", location: "Spain"
-    ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Spain.png"
+    { name: "Rui Costa", img: "https://www.fiware.org/wp-content/directories/people/images/200px/rui-costa.jpg", position: "Chief Executive Officer", company: "Ubiwhere", "company-link": "https://www.ubiwhere.com/",
+    content: "", linkedin: "https://www.linkedin.com/in/ruiarnaldo/", twitter: "", domain: "Gold", location: "Portugal"
+    ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Portugal.png"
     },
     { name: "Angelo Giuliana", img: "https://www.fiware.org/wp-content/directories/people/images/200px/angelo-giuliana.jpg", position: "General Manager", company: "MedITech", "company-link": "https://meditech4.com/",
     content: "Angelo Giuliana has many years of experience in commercial development and sales in IT companies of international scope, where he has held increasing managerial roles up to dealing with EMEA sales management. Giuliana has long-term experience in R&amp;D, has been a consultant to the European Commission for H2020 projects, and has been involved in innovation for some time. His vocation for technology transfer leads him to be a founding member of some innovative startups both in the ICT and manufacturing sectors. Angelo currently plays the role of General Manager of the MISE Competence Center for South Italy – Meditech. He graduated with a degree in Physics in 1982.", linkedin: "https://www.linkedin.com/in/angelogiuliana/", twitter: "https://twitter.com/dirgenmeditech", domain: "Gold", location: "Italy"
     ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Italy.png"
     },
     { name: "Antonio Jara", img: "https://www.fiware.org/wp-content/directories/people/images/200px/antonio-jara.jpg", position: "Chief Scientific Officer", company: "Libelium", "company-link": "https://www.libelium.com/",
-    content: "Antonio has a PhD from the University of Murcia (Spain) and a MBA from the ENAE business school and UCAM (2012). He has received entrepreneurship awards from ENAE, emprendeGo, IPSO Alliance Award for its disruptive innovation in the IoT. As part of HOP Ubiquitous, Jara is focused on the Smart Cities market with solutions for citizens engagements, tourism, active participation, physical web and environmental monitoring (air quality sensors) in projects such as ENIAC SAFESENS, interoperability / pilots (SmartSDK, Synchronicity, Organicity, BeinCPPS) and also in several actions related to security/privacy (INPUT and FORTIKA).", linkedin: "https://www.linkedin.com/in/jara-libelium/", twitter: "https://twitter.com/antonio_jara", domain: "Gold", location: "Spain"
+    content: "", linkedin: "https://www.linkedin.com/in/jara-libelium/", twitter: "https://twitter.com/antonio_jara", domain: "Gold", location: "Spain"
     ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Spain.png"
     },
     { name: "Abdulmajeed Mangara", img: "https://www.fiware.org/wp-content/directories/people/images/200px/abdulmajeed-mangara.jpg", position: "Smart City Program Director", company: "Madinah Region Development Authority", "company-link": "https://www.mda.gov.sa/",
@@ -54,9 +51,5 @@ var companies = [
     { name: "Yasunori Mochizuki", img: "https://www.fiware.org/wp-content/directories/people/images/200px/yasunori-mochizuki.jpg", position: "NEC Fellow", company: "NEC Corporation", "company-link": "https://www.nec.com/",
     content: "Yasunori joined NEC in 1987, straight after concluding his PhD in Electronics Engineering. He went on to spend over two decades at NEC’s corporate R&amp;D center, first as a research scientist and later as a department manager, gaining broader technical expertise, including solid-state physics, semiconductor devices/LSIs, and computer science. Later in 2013, Yasunori started working at NEC’s newly-created Business Innovation Unit. As a senior vice president, he was responsible for the corporate-wide technology strategy and IoT business strategy. His current title of NEC Fellow means that he actively participates in innovation policy proposals, global ecosystem building related to smart society, and digital transformation. He is also a BoD member of FIWARE Foundation, World Economic Forum. Fellow, and member of Business at OECD (BIAC) Committee for Digital Economy Policy.", linkedin: "https://www.linkedin.com/in/yasunori-mochizuki-93bab674/", twitter: "https://twitter.com/yasunorimochiz", domain: "Platinum", location: "Japan"
     ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Japan.png"
-    },
-    { name: "Björn Schwarze", img: "https://www.fiware.org/wp-content/directories/people/images/200px/bjorn-schwarze.jpg", position: "Managing Director", company: "ADDIX", "company-link": "https://www.addix.net/",
-    content: "", linkedin: "https://www.linkedin.com/in/bjoernschw/", twitter: "", domain: "Gold", location: "Germany"
-    ,flag: "https://www.fiware.org/wp-content/directories/people/images/flag/flag_Germany.png"
     }
 ];
